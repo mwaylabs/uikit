958773caa30746c0c149e0eb7dd4bf505b0fac43
azarges <a.zarges@mwaysolutions.com>
2014-02-26 18:49:25 +0100

    implemented that only policies are shown witch were added by the user // finished display differnet version types for policy(information/configuration/aplliedondevices) view // disabled buttons when an old version is selected // remove device from policy implemented // separated leaveConfirmation from form directive that it can be used for other leaveConfirmation as well and not only when the form was changed

--- mwHelper.js
+++ mwHelper.js
@@ -11,17 +11,82 @@ angular.module('mwHelper', [])
  * @description
  * Stops Propagation of specified event for this element
  */
-    .directive('mwStopPropagation', function () {
-      return {
-        restrict: 'A',
-        link: function (scope, elm, attr) {
-          if(!attr.mwStopPropagation) {
-            throw new Error('Directive mwStopPropagation: This directive must have an event name as attribute e.g. mw-stop-propagation="keyup"');
-          }
-          elm.on(attr.mwStopPropagation, function(event) {
-            event.stopPropagation();
-          });
+  .directive('mwStopPropagation', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, elm, attr) {
+        if (!attr.mwStopPropagation) {
+          throw new Error('Directive mwStopPropagation: This directive must have an event name as attribute e.g. mw-stop-propagation="keyup"');
         }
-      };
-    })
-;
+        elm.on(attr.mwStopPropagation, function (event) {
+          event.stopPropagation();
+        });
+      }
+    };
+  })
+
+/**
+ * @ngdoc directive
+ * @name mwForm.directive:mwLeaveConfirmation
+ * @element form
+ * @description
+ *
+ * Opens a confirmation modal when the form has been edited and a the user wants to navigate to a new page
+ *
+ */
+
+  .directive('mwLeaveConfirmation', function ($window, $document, $location, i18n, Modal) {
+    return {
+      scope:{
+        alertBeforeLeave:'=mwLeaveConfirmation'
+      },
+      link: function (scope) {
+
+        var confirmationModal = Modal.create({
+          templateUrl: 'modules/ui/templates/mwForm/mwLeaveConfirmation.html',
+          scope: scope
+        });
+
+        // Prevent the original event so the routing will not be completed
+        // Save the url where it should be navigated to in a temp variable
+        var showConfirmModal = function (ev, next) {
+          if (scope.alertBeforeLeave) {
+            Modal.show(confirmationModal);
+            ev.preventDefault();
+            scope.next = next;
+          }
+        };
+
+        // User wants to stay on the page
+        scope.stay = function () {
+          Modal.hide(confirmationModal);
+        };
+
+        // User really wants to navigate to that page which was saved before in a temp variable
+        scope.continue = function () {
+          if (scope.next) {
+            //instead of scope.$off() we call the original eventhandler function
+            scope.changeLocation();
+
+            //hide the modal and navigate to the page
+            Modal.hide(confirmationModal).then(function () {
+              $window.document.location.href = scope.next;
+              scope.next = null;
+            });
+          }
+        };
+
+        //In case that just a hashchange event was triggered
+        //Angular has no $off event unbinding so the original eventhandler is saved in a variable
+        scope.changeLocation = scope.$on('$locationChangeStart', showConfirmModal);
+
+        //In case that the user clicks the refresh/back button or makes a hard url change
+        $window.onbeforeunload = function () {
+          if (scope.alertBeforeLeave) {
+            return i18n.get('common.confirmModal.description');
+          }
+        };
+
+      }
+    };
+  });
