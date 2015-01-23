ec535a63e932b49a00851297d2f6b39a86132bb9
Zarlex <alex.zarges@online.de>
2015-01-23 17:36:41 +0100

    Added mwFormRadioGroupBb and mwFormSelectBb directives and made it possible to define the optionsKey instead of using the hard coded 'key'

--- mwFormBb.js
+++ mwFormBb.js
@@ -6,16 +6,18 @@ angular.module('mwFormBb', [])
     return {
       restrict: 'A',
       transclude: true,
-      require:'^?form',
+      require: '^?form',
       scope: {
         model: '=',
         collection: '=',
+        mwOptionsKey: '@',
         translationPrefix: '@',
         mwRequired: '=',
         disabledCollection: '='
       },
       templateUrl: 'modules/ui/templates/mwFormBb/mwFormMultiSelect.html',
-      link: function(scope,el,attr,form){
+      link: function (scope, el, attr, form) {
+        scope.optionsKey = scope.mwOptionsKey || 'key';
 
         if (!scope.collection instanceof window.mCAP.Collection) {
           throw new Error('mwFormMultiSelect: collection attribute has to be a collection');
@@ -26,7 +28,7 @@ angular.module('mwFormBb', [])
         }
 
         //When user unselects a checkbox it will be deleted from the model array
-        var removeFromModel = function(key){
+        var removeFromModel = function (key) {
           if (scope.model.indexOf(key) >= 0) {
             // Delete key from model array
             scope.model.splice(scope.model.indexOf(key), 1);
@@ -41,32 +43,98 @@ angular.module('mwFormBb', [])
 
         if (scope.disabledCollection) {
           //if a an item is in the disabledCollection it will be removed from the model
-          scope.disabledCollection.each(function(disabledModel) {
-            removeFromModel(disabledModel.get('key'));
+          scope.disabledCollection.each(function (disabledModel) {
+            removeFromModel(disabledModel.get(scope.optionsKey));
           });
         }
 
-        scope.isDisabled = function(model){
-          if(scope.disabledCollection){
+        scope.isDisabled = function (model) {
+          if (scope.disabledCollection) {
             return !!scope.disabledCollection.get(model);
           }
         };
 
         scope.toggleKeyIntoModelArray = function (key) {
           scope.model = scope.model || [];
-          if(!removeFromModel(key)){
+          if (!removeFromModel(key)) {
             scope.model.push(key);
           }
         };
 
-        scope.showRequiredMessage = function(){
-          return ( (!scope.model || scope.model.length<1) && scope.mwRequired);
+        scope.showRequiredMessage = function () {
+          return ( (!scope.model || scope.model.length < 1) && scope.mwRequired);
         };
 
-        scope.setDirty = function(){
-          if(form){
+        scope.setDirty = function () {
+          if (form) {
             form.$setDirty();
           }
+        };
+      }
+    };
+  })
+
+  .directive('mwFormRadioGroupBb', function () {
+    return {
+      restrict: 'A',
+      transclude: true,
+      require: '^?form',
+      scope: {
+        mwModel: '=',
+        mwOptionsCollection: '=',
+        mwOptionsKey: '@',
+        mwOptionsLabelKey: '@',
+        mwOptionsLabelI18nPrefix: '@',
+        mwRequired: '=',
+        mwDisabled: '=',
+        name: '@'
+      },
+      templateUrl: 'modules/ui/templates/mwFormBb/mwFormRadioGroup.html',
+      link: function (scope ) {
+        scope.optionsKey = scope.mwOptionsKey || 'key';
+
+        if (scope.mwOptionsCollection.length === 0) {
+          scope.mwOptionsCollection.fetch();
+        }
+      }
+    };
+  })
+
+  .directive('mwFormSelectBb', function (i18n) {
+    return {
+      restrict: 'A',
+      transclude: true,
+      require: '^?form',
+      scope: {
+        mwModel: '=',
+        mwOptionsCollection: '=',
+        mwOptionsKey: '@',
+        mwOptionsLabelKey: '@',
+        mwOptionsLabelI18nPrefix: '@',
+        mwRequired: '=',
+        mwDisabled: '=',
+        mwChange: '&',
+        name: '@',
+        placeholder: '@'
+      },
+      templateUrl: 'modules/ui/templates/mwFormBb/mwFormSelect.html',
+      link: function (scope ) {
+        scope.optionsKey = scope.mwOptionsKey || 'key';
+
+        if (scope.mwOptionsCollection.length === 0) {
+          scope.mwOptionsCollection.fetch();
+        }
+
+        scope.change = function(){
+
+        };
+
+        scope.getKey = function(optionModel){
+          return optionModel.get(scope.optionsKey);
+        };
+
+        scope.getLabel = function(optionModel){
+          return i18n.get(scope.mwOptionsLabelI18nPrefix+'.'+scope.getKey(optionModel));
         };
       }
     };
