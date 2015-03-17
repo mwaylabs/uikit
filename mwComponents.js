83a4634081d93d0cb09fc83022667ab390e2eed7
pwambach <philippwambach@googlemail.com>
2015-03-17 12:07:45 +0100

    adds mwCollapsable directive

--- mwComponents.js
+++ mwComponents.js
@@ -903,6 +903,33 @@ angular.module('mwComponents', [])
         });
       }
     };
+  })
+
+
+  .directive('mwCollapsable', function(){
+    return {
+      transclude: true,
+      scope: {
+        mwCollapsable: '=',
+        title: '@mwTitle'
+      },
+      templateUrl: 'modules/ui/templates/mwComponents/mwCollapsable.html',
+      link: function (scope, elm) {
+        scope.viewModel = {};
+        scope.viewModel.collapsed = false;
+        if(scope.mwCollapsable === false){
+          scope.viewModel.collapsed = true;
+        }
+        var level = elm.parents('.mw-collapsable').length;
+        if(level){
+          elm.css('margin-left', level*20+'px');
+        }
+
+        scope.toggle = function(){
+          scope.viewModel.collapsed = !scope.viewModel.collapsed;
+        };
+      }
+    };
   });
 
 
