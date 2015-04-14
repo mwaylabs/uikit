571e94d466131ef3999f514554f173efd7f9c4b8
Zarlex <alex.zarges@online.de>
2015-04-14 15:00:42 +0200

    adds new icons from relution master font

--- mwListable.js
+++ mwListable.js
@@ -409,7 +409,7 @@ angular.module('mwListable', [])
         scope: {
           link: '@mwListableLinkEdit'
         },
-        template: '<a ng-href="{{ link }}" class="btn btn-default btn-sm"><span mw-icon="pencil"></span></a>',
+        template: '<a ng-href="{{ link }}" class="btn btn-default btn-sm"><span mw-icon="rln-icon edit"></span></a>',
         link: function (scope, elm, attr, mwListableCtrl) {
           mwListableCtrl.actionColumns.push(null);
         }
