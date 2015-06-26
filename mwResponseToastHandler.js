c0708638c8b4a7da8d04542b350e62bdf6b19d01
Zarlex <alex.zarges@online.de>
2015-06-26 17:51:15 +0200

    adds mwToast and mwResponseToast handler

---
+++ mwResponseToastHandler.js
@@ -0,0 +1,80 @@
+/**
+ * Created by zarges on 23/06/15.
+ */
+'use strict';
+angular.module('mwResponseToastHandler', ['mwResponseHandler', 'mwI18n', 'mwToast'])
+
+  .provider('ResponseToastHandler', function ($provide, ResponseHandlerProvider) {
+    var _registeredIds = [],
+        _registeredToastOptions = {
+          DEFAULT: {
+            type: 'default',
+            autoHide: false
+          }
+        };
+
+    var _getNoftificationCallback = function (messages, id, options) {
+      options = options || {};
+      var factoryName = _.uniqueId('notification_factory');
+      $provide.factory(factoryName, ['Toast', 'i18n', function (Toast, i18n) {
+        return function (response) {
+          var prevToast = Toast.findToast(id),
+            data = response.data || {},
+            messageStr = prevToast ? messages.plural : messages.singular,
+            message,
+            toastOptions = {
+              id: id
+            };
+
+          data.$count = prevToast ? prevToast.replaceCount+1 : 0;
+          data.$count++;
+
+          if(options.preProcess && typeof options.preProcess=== 'function'){
+            message = options.preProcess.call(this, messageStr, data, i18n);
+          } else {
+            if(data.results && data.results.length > 0 ){
+              data = data.results[0];
+            }
+            message = i18n.get(messageStr, data);
+          }
+
+          if(options.toastType){
+            var opts = _registeredToastOptions[options.toastType];
+            if(opts){
+              _.extend(toastOptions, opts);
+            } else {
+              throw new Error('Type '+options.toastType+' is not available. Make sure you have configured it first')
+            }
+          } else {
+            _.extend(toastOptions, _registeredToastOptions.DEFAULT);
+          }
+
+          Toast.addToast(message, toastOptions);
+        };
+      }]);
+      return factoryName;
+    };
+
+    this.registerToastType = function(typeId, toastOptions){
+      if(_registeredToastOptions[typeId]){
+
+      } else {
+        _registeredToastOptions[typeId] = toastOptions;
+      }
+    };
+
+    this.registerToast = function (route, messages, options) {
+      var msgId = options.id || route + '_' + options.method,
+        callbackFactory = _getNoftificationCallback(messages, msgId, options);
+
+      if(_registeredIds.indexOf(msgId)>-1){
+        throw new Error('You can not define a second message for the route '+route+' and method '+ options.method + ' because you have already registered one!');
+      } else {
+        _registeredIds.push(msgId);
+        return ResponseHandlerProvider.registerAction(route, callbackFactory, options);
+      }
+    };
+
+    this.$get = function () {};
+
+  });
---
+++ mwToast.js
@@ -0,0 +1,146 @@
+'use strict';
+
+angular.module('mwToast', [])
+
+  .directive('mwToasts', function (Toast) {
+    return {
+      templateUrl: 'modules/ui/templates/mwToast/mwToasts.html',
+      link: function (scope) {
+        scope.toasts = Toast.getToasts();
+
+        scope.hideToast = function (toast) {
+          Toast.removeToast(toast);
+        };
+
+      }
+    };
+  })
+
+  .provider('Toast', function () {
+
+    var _autoHideTime = 5000,
+      _toasts = [];
+
+    var Toast = function (message, options) {
+      options = options || {};
+      options.button = options.button || {};
+
+      var replaceMessage = function (newMessage) {
+        toast.message = newMessage;
+        toast.replaceCount++;
+        resetAutoHideTimer();
+      };
+
+      var setAutoHideCallback = function(fn){
+        toast.autoHide = true;
+        toast.autoHideCallback = fn;
+        resetAutoHideTimer();
+      };
+
+      var resetAutoHideTimer = function () {
+        window.clearTimeout(_autoRemoveTimeout);
+        startAutoHideTimer();
+      };
+
+      var startAutoHideTimer = function () {
+        if (toast.autoHide) {
+          _autoRemoveTimeout = window.setTimeout(function () {
+            if (toast.autoHideCallback && typeof toast.autoHideCallback === 'function') {
+              toast.visible = false;
+              toast.autoHideCallback.apply(this, arguments);
+            }
+          }.bind(this), toast.autoHideTime);
+        }
+      };
+
+      var toast = {
+          id: options.id || _.uniqueId('toast'),
+          type: options.type || 'default',
+          visible: true,
+          message: message,
+          autoHide: options.autoHide,
+          autoHideTime: options.autoHideTime || 5000,
+          autoHideCallback: options.autoHideCallback,
+          isHtmlMessage: options.isHtmlMessage,
+          button: {
+            title: options.button.title,
+            link: options.button.link,
+            isLink: !!options.button.link,
+            action: options.button.action
+          },
+          replaceMessage: replaceMessage,
+          replaceCount: 0,
+          setAutoHideCallback: setAutoHideCallback
+        },
+        _autoRemoveTimeout;
+
+      startAutoHideTimer();
+
+      return toast;
+    };
+
+    this.setAutoHideTime = function(timeInMs){
+      _autoHideTime = timeInMs;
+    };
+
+    this.$get = function ($timeout) {
+
+      return {
+        findToast: function (id) {
+          var toastContainer = _.findWhere(_toasts, {id: id});
+          if(toastContainer){
+            return toastContainer.toast;
+          } else {
+            return false;
+          }
+        },
+        getToasts: function () {
+          return _.pluck(_toasts, 'toast');
+        },
+        replaceToast: function (toast, message) {
+
+          var toast = this.findToast(toast.id);
+
+          if (toast) {
+            toast.replaceMessage(message);
+          }
+
+          return toast;
+        },
+        removeToast: function (toast) {
+          var match = this.findToast(toast),
+            index = _.indexOf(_toasts, match);
+          if (index) {
+            _toasts.splice(index, 1);
+          }
+          return !!index;
+        },
+        addToast: function (message, options) {
+          options = options || {};
+
+          options.autoHideTime = options.autoHideTime || _autoHideTime;
+
+          var existingToast = this.findToast(options.id);
+
+          if(existingToast){
+            this.replaceToast(existingToast, message);
+          } else {
+            var toast = new Toast(message, options);
+
+            var removeFn = function () {
+              $timeout(function () {
+                if (options.autoHideCallback && typeof options.autoHideCallback === 'function') {
+                  options.autoHideCallback.apply(this, arguments);
+                }
+                this.removeToast(toast);
+              });
+            }.bind(this);
+
+            toast.setAutoHideCallback(removeFn);
+
+            _toasts.push({id: toast.id, toast: toast});
+          }
+        }
+      };
+    };
+  });
