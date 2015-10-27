e01a407dcb64a92409e708d39a891da596f44536
Zarlex <alex.zarges@online.de>
2015-10-27 17:01:49 +0100

    refactors filter logic

--- src/mwSidebarBb.js
+++ src/mwSidebarBb.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('mwSidebarBb', [])
-
 /**
  * @ngdoc directive
  * @name mwSidebar.directive:mwSidebarFilters
@@ -11,53 +10,157 @@ angular.module('mwSidebarBb', [])
  * Container for filters
  *
  */
-  .directive('mwSidebarFiltersBb', function (Persistance, EmptyState) {
+  .directive('mwSidebarFiltersBb', function ($timeout, MCAPFilterHolder) {
     return {
       transclude: true,
       templateUrl: 'uikit/templates/mwSidebarBb/mwSidebarFilters.html',
-      controller: function($scope){
-        this.getCollection = function(){
+      controller: function ($scope) {
+        this.getCollection = function () {
           return $scope.collection;
         };
 
-        this.changeFilter = function(property, value, isUrlParam){
-          if(isUrlParam){
+        this.getFilterHolders = function () {
+          return $scope.filterHolders;
+        };
+
+        this.changeFilter = function (property, value, isUrlParam) {
+          if (isUrlParam) {
             $scope.collection.customUrlParams[property] = value;
           } else {
             var filterVal = {};
             filterVal[property] = value;
             $scope.collection.filterable.setFilters(filterVal);
+            $scope.viewModel.tmpFilter.set({filter: $scope.collection.filterable.getFilters()});
+            $scope.viewModel.tmpFilter.get('filterValues')[property] = value;
           }
-
-          EmptyState.pushFilter($scope.collection, property);
-
-          //persist filter values
-          Persistance.saveFilterValues($scope.collection);
 
           $scope.collection.fetch();
 
-        }
-      },
-      link: function (scope, elm, attr) {
-
-        scope.collection = scope.$eval(attr.collection);
-        if(!angular.isDefined(scope.collection)){
-          throw new Error('mwSidebarFiltersBb does not have a collection!');
-        }
-
-        scope.resetFiltersOnClose = function () {
-          if (!scope.toggleFilters) {
-            scope.collection.filterable.resetFilters();
-            Persistance.clearFilterValues(scope.collection);
-            scope.collection.fetch().then(function(collection){
-              EmptyState.resetFilter(collection);
-            });
-          }
         };
+      },
+      link: function (scope, el, attr) {
 
-        //open filters when there are persisted filters saved
-        if(Persistance.getFilterValues(scope.collection)){
-          scope.toggleFilters = true;
+        scope.mwListCollection = scope.$eval(attr.mwListCollection);
+        scope.collection = scope.$eval(attr.collection);
+
+        if (scope.mwListCollection) {
+
+          var _filterAnimationDuration = 400;
+
+          scope.collection = scope.mwListCollection.getCollection();
+          scope.mwListCollectionFilter = scope.mwListCollection.getMwListCollectionFilter();
+          scope.filters = scope.mwListCollectionFilter.getFilters();
+          scope.appliedFilter = scope.mwListCollectionFilter.getAppliedFilter();
+
+          scope.viewModel = {
+            tmpFilter: new MCAPFilterHolder(),
+            showFilterForm: false,
+            canShowForm: false
+          };
+
+          var setTotalAmount = function (filterModel) {
+            var filterModelInCollection = scope.filters.get(filterModel),
+              totalAmount = scope.collection.filterable.getTotalAmount();
+
+            filterModel.set('totalAmount', totalAmount);
+            if (filterModelInCollection) {
+              filterModelInCollection.set('totalAmount', totalAmount);
+            }
+          };
+
+          var filterCollection = function (filterModel) {
+            scope.collection.filterable.resetFilters();
+            scope.collection.filterable.setFilters(filterModel.get('filterValues'));
+            return scope.collection.fetch().then(function () {
+              setTotalAmount(filterModel);
+            });
+          };
+
+          scope.saveFilter = function () {
+            var filter;
+            if (scope.viewModel.tmpFilter.isNew()) {
+              filter = new MCAPFilterHolder(scope.viewModel.tmpFilter.toJSON());
+            } else {
+              filter = scope.viewModel.tmpFilter;
+            }
+            scope.mwListCollectionFilter.saveFilter(filter).then(function (filterModel) {
+              scope.viewModel.showFilterForm = false;
+              scope.applyFilter(filterModel);
+            });
+
+          };
+
+          scope.deleteFilter = function (filterModel) {
+            var removeId = filterModel.id,
+                appliedId = scope.appliedFilter.id;
+
+            return scope.mwListCollectionFilter.deleteFilter(filterModel).then(function () {
+
+              if (removeId === appliedId) {
+                scope.revokeFilter();
+              }
+            });
+          };
+
+          scope.applyFilter = function (filterModel) {
+            filterCollection(filterModel).then(function () {
+              scope.mwListCollectionFilter.applyFilter(filterModel);
+            });
+          };
+
+          scope.revokeFilter = function () {
+            scope.mwListCollectionFilter.revokeFilter().then(function(){
+              scope.collection.filterable.resetFilters();
+              scope.collection.fetch();
+              scope.appliedFilter.clear();
+            });
+          };
+
+          scope.addFilter = function () {
+            var emptyFilter = new MCAPFilterHolder();
+
+            scope.viewModel.canShowForm = true;
+            scope.viewModel.tmpFilter.clear();
+            scope.viewModel.tmpFilter.set(emptyFilter.toJSON());
+            scope.viewModel.showFilterForm = true;
+            $timeout(function(){
+              filterCollection(scope.viewModel.tmpFilter);
+            },_filterAnimationDuration);
+          };
+
+          scope.editFilter = function (filterModel) {
+            scope.viewModel.canShowForm = true;
+            scope.viewModel.tmpFilter.clear();
+            scope.viewModel.tmpFilter.set(filterModel.toJSON());
+            scope.viewModel.showFilterForm = true;
+            $timeout(function(){
+              filterCollection(filterModel);
+            },_filterAnimationDuration);
+          };
+
+          scope.cancelFilterEdit = function () {
+            scope.viewModel.showFilterForm = false;
+            if (scope.appliedFilter.id !== scope.viewModel.tmpFilter.id) {
+              $timeout(function(){
+                scope.applyFilter(scope.appliedFilter);
+              },_filterAnimationDuration);
+            }
+          };
+
+          scope.mwListCollectionFilter.fetchFilters();
+          scope.mwListCollectionFilter.fetchAppliedFilter().then(function (filterModel) {
+            setTotalAmount(filterModel);
+          });
+
+        } else if (scope.collection) {
+          // TODO ADD OLD IMPLEMENTATION
+          console.warn('The scope attribute collection is deprecated please use the mwCollection instead');
+          scope.viewModel = {
+            showFilterForm: true,
+            canShowForm: true
+          };
+        } else {
+          throw new Error('please pass a collection or mwCollection as scope attribute');
         }
       }
     };
@@ -102,51 +205,57 @@ angular.module('mwSidebarBb', [])
         customUrlParameter: '@'
       },
       templateUrl: 'uikit/templates/mwSidebarBb/mwSidebarSelect.html',
-      link: function (scope, elm, attr, ctrl) {
+      link: function (scope, elm, attr, mwSidebarFiltersBbCtrl) {
 
         scope.viewModel = {};
 
         //set key function for select key
-        scope.key = function(model) {
-          if(angular.isDefined(scope.keyProperty)){
+        scope.key = function (model) {
+          if (angular.isDefined(scope.keyProperty)) {
             return model.attributes[scope.keyProperty];
           } else {
             return model.attributes.key;
           }
         };
 
+        scope.collection = mwSidebarFiltersBbCtrl.getCollection();
+
         //set label function fo select label
-        scope.label = function(model){
+        scope.label = function (model) {
           //translate with i18n service if translationPrefix is defined
           var label = scope.key(model);
-          if(scope.translationPrefix && scope.translationSuffix){
-            label = i18n.get(scope.translationPrefix + '.' + scope.key(model) + '.'+scope.translationSuffix);
-          } else if(scope.translationSuffix){
-            label = i18n.get(scope.key(model)+'.'+scope.translationSuffix);
-          } else if(scope.translationPrefix){
+          if (scope.translationPrefix && scope.translationSuffix) {
+            label = i18n.get(scope.translationPrefix + '.' + scope.key(model) + '.' + scope.translationSuffix);
+          } else if (scope.translationSuffix) {
+            label = i18n.get(scope.key(model) + '.' + scope.translationSuffix);
+          } else if (scope.translationPrefix) {
             label = i18n.get(scope.translationPrefix + '.' + scope.key(model));
           }
           return label;
         };
 
-        scope.changed = function(){
+        scope.changed = function () {
           var property = scope.customUrlParameter ? scope.customUrlParameter : scope.property;
-          ctrl.changeFilter(property, scope.viewModel.val, !!scope.customUrlParameter );
+          mwSidebarFiltersBbCtrl.changeFilter(property, scope.viewModel.val, !!scope.customUrlParameter);
         };
 
-        if(angular.isDefined(scope.labelProperty)){
-          scope.label = function(model){
+        if (angular.isDefined(scope.labelProperty)) {
+          scope.label = function (model) {
             //translate if value is a translation object
-            if(angular.isObject(model.attributes[scope.labelProperty])){
+            if (angular.isObject(model.attributes[scope.labelProperty])) {
               return i18n.localize(model.attributes[scope.labelProperty]);
             }
             return model.attributes[scope.labelProperty];
           };
         }
 
-        if(angular.isDefined(scope.labelTransformFn)){
+        if (angular.isDefined(scope.labelTransformFn)) {
           scope.label = scope.labelTransformFn;
         }
+
+        scope.$watch('collection.filterable.filterValues.' + scope.property, function (val) {
+          scope.viewModel.val = val;
+        });
       }
     };
   })
@@ -167,7 +276,7 @@ angular.module('mwSidebarBb', [])
  * @param {string} customUrlParameter If set, the filter will be set as a custom url parameter in the collection's filterable
  */
 
-  .directive('mwSidebarNumberInputBb', function (Persistance, EmptyState) {
+  .directive('mwSidebarNumberInputBb', function () {
     return {
       require: '^mwSidebarFiltersBb',
       scope: {
@@ -183,13 +292,13 @@ angular.module('mwSidebarBb', [])
 
         scope.viewModel = {};
 
-        scope.isValid = function(){
+        scope.isValid = function () {
           return elm.find('input').first().hasClass('ng-valid');
         };
 
-        scope.changed = function(){
+        scope.changed = function () {
           var property = scope.customUrlParameter ? scope.customUrlParameter : scope.property;
-          ctrl.changeFilter(property, scope.viewModel.val, !!scope.customUrlParameter );
+          ctrl.changeFilter(property, scope.viewModel.val, !!scope.customUrlParameter);
         };
       }
     };
