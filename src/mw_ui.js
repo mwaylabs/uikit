ee4aea601adf20747717471902f124eee6567eac
Zarlex <alex.zarges@online.de>
2016-02-23 16:46:59 +0100

    adds new modules

--- src/mw_ui.js
+++ src/mw_ui.js
@@ -4,16 +4,23 @@
   angular.module('mwUI', [
     'mwUI.Inputs',
     'mwUI.i18n',
-    'mwUI.Layout'
+    'mwUI.Layout',
+    'mwUI.Menu',
+    'mwUI.UiComponents',
+    'mwUI.Utils'
   ]);
 
   root.mwUI = {};
 
 // @include ../.tmp/templates.js
 
+// @include ./mw-utils/mw_utils.js
+
+// @include ./mw-backbone/mw_backbone.js
 // @include ./mw-i18n/mw_i18n.js
 // @include ./mw-inputs/mw_inputs.js
 // @include ./mw-layout/mw_layout.js
+// @include ./mw-menu/mw_menu.js
 // @include ./mw-ui-components/mw_ui_components.js
 
 })(window, angular);
