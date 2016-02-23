b121298ff2792cbe6722ba913ea500ddbc341612
Zarlex <alex.zarges@online.de>
2016-02-23 16:43:17 +0100

    removes logic that didn't work properly and uses css pseudo-selector :invalid instead

--- src/mw-inputs/directives/mw_select.js
+++ src/mw-inputs/directives/mw_select.js
@@ -3,26 +3,13 @@ angular.module('mwUI.Inputs')
   .directive('mwSelect', function () {
     return {
       require: '^?ngModel',
-      link: function (scope, el, attrs, ngModel) {
+      link: function (scope, el) {
         var customSelectWrapper = angular.element('<span class="custom-select mw-select"></span>');
 
         var render = function () {
           el.wrap(customSelectWrapper);
           el.addClass('custom');
         };
-
-        scope.$watch(
-          function () {
-            return ngModel.$modelValue;
-          },
-          function (val) {
-            if (angular.isUndefined(val)) {
-              el.addClass('default-selected');
-            } else {
-              el.removeClass('default-selected');
-            }
-          }
-        );
 
         render();
       }
