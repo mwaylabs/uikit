c7e7d1c7eeffafec9a42e2505f28610c84e3ebd0
Zarlex <alex.zarges@online.de>
2016-02-23 16:39:31 +0100

    adds mwMenu entry logic

---
+++ src/mw-menu/collections/mw_menu.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zarges on 15/02/16.
+ */
+var MwMenu = window.mwUI.Menu.MwMenuEntries.extend({});
+
+window.mwUI.Menu.MwMenu = MwMenu;
---
+++ src/mw-menu/collections/mw_menu_sub_entries.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zarges on 15/02/16.
+ */
+var MwMenuSubEntries = window.mwUI.Menu.MwMenuEntries.extend({});
+
+window.mwUI.Menu.MwMenuSubEntries = MwMenuSubEntries;
