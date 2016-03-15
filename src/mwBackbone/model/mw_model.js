2e3ff5c18f87de795c6a0f5734c641cdd1707079
Zarlex <alex.zarges@online.de>
2016-03-15 13:47:09 +0100

    adds request method to model

--- src/mwBackbone/model/mw_model.js
+++ src/mwBackbone/model/mw_model.js
@@ -8,20 +8,33 @@ mwUI.Backbone.Model = mwUI.Backbone.NestedModel.extend({
       endpoint = _.result(this, 'endpoint');
 
     if (endpoint) {
-      return window.mwUI.Backbone.concatUrlParts(mwUI.Backbone.baseUrl,basePath,endpoint);
+      return window.mwUI.Backbone.concatUrlParts(mwUI.Backbone.baseUrl, basePath, endpoint);
     } else {
       throw new Error('An endpoint has to be specified');
     }
   },
+  constructor: function () {
+    var superConstructor = mwUI.Backbone.NestedModel.prototype.constructor.apply(this, arguments);
+    mwUI.Backbone.SelectableModel.prototype.selectableModelConstructor.apply(this, arguments);
+    return superConstructor;
+  },
   getEndpoint: function () {
     return this.urlRoot();
   },
-  setEndpoint: function(endpoint){
+  setEndpoint: function (endpoint) {
     this.endpoint = endpoint;
   },
-  constructor: function () {
-    var superConstructor = mwUI.Backbone.NestedModel.prototype.constructor.apply(this,arguments);
-    mwUI.Backbone.SelectableModel.prototype.selectableModelConstructor.apply(this,arguments);
-    return superConstructor;
+  sync: function (method, model, options) {
+    options.instance = this;
+    return mwUI.Backbone.NestedModel.prototype.sync.call(this, method, model, options);
+  },
+  request: function (url, method, options) {
+    options = options || {};
+    var requestOptions = {
+      url: url,
+      type: method,
+      instance: this
+    };
+    return Backbone.ajax(_.extend(requestOptions, options));
   }
 });
