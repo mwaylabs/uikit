e712951794292ff6eec5219bc929b96dbb928323
Axel Hodler <derxor@gmail.com>
2016-03-29 15:01:38 +0200

    MDM-10640: create ignore key press service

---
+++ test/mwComponents/mw-components-bb.js
@@ -0,0 +1,40 @@
+describe('mwComponentsBb', function() {
+  var ignoreKeyPress;
+
+  beforeEach(function() {
+    module('mwComponentsBb');
+  });
+
+  beforeEach(inject(function(_ignoreKeyPress_) {
+    ignoreKeyPress = _ignoreKeyPress_;
+  }));
+
+  describe('ignoreKeyPress', function() {
+    var event;
+
+    beforeEach(function() {
+      event = {
+        defaultPrevented : false,
+        preventDefault: function() {
+          this.defaultPrevented = true;
+        }
+      }
+    });
+
+    it('can ignore the enter key', function() {
+      event.which = 13;
+
+      ignoreKeyPress.ignoreEnterKey(event);
+
+      expect(event.defaultPrevented).toBe(true);
+    });
+
+    it('does not ignore non enter key', function() {
+      event.which = 14;
+
+      ignoreKeyPress.ignoreEnterKey(event);
+
+      expect(event.defaultPrevented).toBe(false);
+    });
+  });
+})
