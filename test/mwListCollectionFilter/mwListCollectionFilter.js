afb98b5e961dff9e31007130a4889133bbec830b
Axel Hodler <derxor@gmail.com>
2016-04-01 15:05:22 +0200

    MDM-7712: improve test readability

--- test/mwListCollectionFilter/mwListCollectionFilter.js
+++ test/mwListCollectionFilter/mwListCollectionFilter.js
@@ -31,11 +31,7 @@ describe('MwListCollectionFilter', function() {
     spyOn(MCAPFilterHolderSpy, 'set');
 
     $provide.value('MCAPFilterHolderProvider', MCAPFilterHolderProviderSpy);
-
-    function MCAPFilterHolders(irrelevant, type) {
-      this.type = type;
-    };
-    $provide.value('MCAPFilterHolders', MCAPFilterHolders);
+    $provide.value('MCAPFilterHolders', function MCAPFilterHolders(irrelevant, type) { this.type = type; });
 
     MCAPAuthenticatedUserSpy = { get: function() {} };
     spyOn(MCAPAuthenticatedUserSpy, 'get').and.returnValue(currentUserUuid);
@@ -47,45 +43,49 @@ describe('MwListCollectionFilter', function() {
   }));
 
   describe('fetching applied filters', function() {
+    var listCollectionFilter;
+
+    beforeEach(function() {
+      listCollectionFilter = new MwListCollectionFilter('IRRELEVANT');
+    });
+
     it('returns null if no applied filter is in localstorage', function() {
       filterInLocalStorage = null;
 
-      var appliedFilter = new MwListCollectionFilter('IRRELEVANT').fetchAppliedFilter();
+      var appliedFilter = listCollectionFilter.fetchAppliedFilter();
 
       expect(appliedFilter).toBeNull();
     });
 
     it('returns filter in localstorage if present', function() {
-      filterInLocalStorage = '{"content":"IRRELEVANT"}';
+      filterInLocalStorage = 'IRRELEVANT';
 
-      var appliedFilter = new MwListCollectionFilter('IRRELEVANT').fetchAppliedFilter();
+      var appliedFilter = listCollectionFilter.fetchAppliedFilter();
 
       expect(appliedFilter).not.toBeNull();
     });
 
     it('does not set filter if the one in localstorage does not contain current users id', function() {
       var filterInLocalStorage = {
-        content:"IRRELEVANT",
         acl: "172FB965-64B2-4B6A-BF8C-679B02460B7B:rw"
       };
       currentUserUuid = 'DFD04C8B-519A-4D0A-BE60-F47EB4D563E8';
 
-      var appliedFilter = new MwListCollectionFilter('IRRELEVANT')
-        ._setAppliedFilter(filterInLocalStorage);
+      var appliedFilter = listCollectionFilter._setAppliedFilter(filterInLocalStorage);
 
+      expect(appliedFilter).not.toBeNull();
       expect(MCAPFilterHolderSpy.set).not.toHaveBeenCalledWith(filterInLocalStorage);
     });
 
     it('sets appliedfilter if current user uuid matches with the filter owner', function() {
       currentUserUuid = 'DFD04C8B-519A-4D0A-BE60-F47EB4D563E8';
       var filterInLocalStorage = {
-        content:"IRRELEVANT",
         acl: currentUserUuid + ":rw"
       };
 
-      var appliedFilter = new MwListCollectionFilter('IRRELEVANT')
-        ._setAppliedFilter(filterInLocalStorage);
+      var appliedFilter = listCollectionFilter._setAppliedFilter(filterInLocalStorage);
 
+      expect(appliedFilter).not.toBeNull();
       expect(MCAPFilterHolderSpy.set).toHaveBeenCalledWith(filterInLocalStorage);
     });
   });
