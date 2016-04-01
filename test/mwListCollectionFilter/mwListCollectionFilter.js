1811ba8cd1823ab0fb881886e699d755553096b0
gitbreaker222 <ruben.labiunda@openmailbox.org>
2016-04-01 18:20:49 +0200

    MDM-7712 restructures and comments test

--- test/mwListCollectionFilter/mwListCollectionFilter.js
+++ test/mwListCollectionFilter/mwListCollectionFilter.js
@@ -11,8 +11,9 @@ describe('MwListCollectionFilter', function() {
   beforeEach(module('mwCollection'));
 
   beforeEach(module(function($provide) {
-    var LocalForage = {
-      getItem: function(identifier) {
+    //create stubs to mimic external input-methods from the real context
+    var LocalForageStub = {
+      getItem: function(dummyParameter) {
         return {
           then: function() {
             return filterInLocalStorage;
@@ -20,27 +21,41 @@ describe('MwListCollectionFilter', function() {
         };
       }
     };
-    $provide.value('LocalForage', LocalForage);
 
+    var MCAPFilterHolderStub = function (dummy, type) {
+      this.type = type;
+    };
+
+    //create spies to mimic output-methods to external real context
     MCAPFilterHolderProviderSpy = { createFilterHolder: function() {}};
+
     MCAPFilterHolderSpy = {
       get: function() {},
       set: function(value) {}
     };
+
+    MCAPAuthenticatedUserSpy = {
+      get: function() {}
+    };
+
+    //redirect calls-to-this-external-methods to the stubs / spies
+    $provide.value('LocalForage', LocalForageStub);
+    $provide.value('MCAPFilterHolders', MCAPFilterHolderStub);
+    $provide.value('MCAPFilterHolderProvider', MCAPFilterHolderProviderSpy);
+    $provide.value('MCAPauthenticatedUser', MCAPAuthenticatedUserSpy);
+
+    //spy on these methods to see if they get called
     spyOn(MCAPFilterHolderProviderSpy, 'createFilterHolder').and.returnValue(MCAPFilterHolderSpy);
     spyOn(MCAPFilterHolderSpy, 'set');
-
-    $provide.value('MCAPFilterHolderProvider', MCAPFilterHolderProviderSpy);
-    $provide.value('MCAPFilterHolders', function MCAPFilterHolders(irrelevant, type) { this.type = type; });
-
-    MCAPAuthenticatedUserSpy = { get: function() {} };
     spyOn(MCAPAuthenticatedUserSpy, 'get').and.returnValue(currentUserUuid);
-    $provide.value('MCAPauthenticatedUser', MCAPAuthenticatedUserSpy);
   }));
+
 
   beforeEach(inject(function(_MwListCollectionFilter_) {
     MwListCollectionFilter = _MwListCollectionFilter_;
   }));
+
+
 
   describe('fetching applied filters', function() {
     var listCollectionFilter;
@@ -67,7 +82,7 @@ describe('MwListCollectionFilter', function() {
 
     it('does not set filter if the one in localstorage does not contain current users id', function() {
       var filterInLocalStorage = {
-        acl: "172FB965-64B2-4B6A-BF8C-679B02460B7B:rw"
+        acl: '172FB965-64B2-4B6A-BF8C-679B02460B7B:rw'
       };
       currentUserUuid = 'DFD04C8B-519A-4D0A-BE60-F47EB4D563E8';
 
@@ -80,7 +95,7 @@ describe('MwListCollectionFilter', function() {
     it('sets appliedfilter if current user uuid matches with the filter owner', function() {
       currentUserUuid = 'DFD04C8B-519A-4D0A-BE60-F47EB4D563E8';
       var filterInLocalStorage = {
-        acl: currentUserUuid + ":rw"
+        acl: currentUserUuid + ':rw'
       };
 
       var appliedFilter = listCollectionFilter._setAppliedFilter(filterInLocalStorage);
