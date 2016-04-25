881da3194c381212d4f880855e4a2430eb85b948
Zarlex <alex.zarges@online.de>
2016-04-25 23:30:07 +0200

    adds new modules

--- src/mw_ui.js
+++ src/mw_ui.js
@@ -2,13 +2,28 @@
   'use strict';
 
   angular.module('mwUI', [
-    'mwUI.Inputs',
-    'mwUI.i18n',
-    'mwUI.Layout',
-    'mwUI.Menu',
-    'mwUI.UiComponents',
-    'mwUI.Utils'
-  ]);
+      'mwUI.Inputs',
+      'mwUI.i18n',
+      'mwUI.Layout',
+      'mwUI.List',
+      'mwUI.Menu',
+      'mwUI.Modal',
+      'mwUI.Toast',
+      'mwUI.UiComponents',
+      'mwUI.Utils'
+    ])
+
+    .config(function (i18nProvider) {
+      i18nProvider.addLocale('de_DE', 'Deutsch', 'de_DE.json');
+      i18nProvider.addLocale('en_US', 'English (US)', 'en_US.json');
+    })
+
+    .run(function(i18n){
+      i18n.setLocale('en_US');
+    });
+
+  //This is only for backwards compatibility and should not be used
+  window.mCAP = window.mCAP || {};
 
   root.mwUI = {};
 
@@ -20,7 +35,10 @@
 // @include ./mw-i18n/mw_i18n.js
 // @include ./mw-inputs/mw_inputs.js
 // @include ./mw-layout/mw_layout.js
+// @include ./mw-list/mw_list.js
 // @include ./mw-menu/mw_menu.js
+// @include ./mw-modal/mw_modal.js
+// @include ./mw-toast/mw_toast.js
 // @include ./mw-ui-components/mw_ui_components.js
 
 })(window, angular);
