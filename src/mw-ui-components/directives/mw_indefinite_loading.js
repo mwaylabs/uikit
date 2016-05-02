eba06cc3380674e44636ab6c8700981debf90400
Zarlex <alex.zarges@online.de>
2016-05-02 16:37:02 +0200

    adds indefinite loader

---
+++ src/mw-ui-components/directives/mw_indefinite_loading.js
@@ -0,0 +1,7 @@
+angular.module('mwUI.UiComponents')
+
+  .directive('mwIndefiniteLoading', function () {
+    return {
+      templateUrl: 'uikit/mw-ui-components/directives/templates/mw_indefinite_loading.html'
+    };
+  });
