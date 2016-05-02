e2f221daf43ca6125ebec7ef893695a60c94cf7b
Zarlex <alex.zarges@online.de>
2016-05-02 18:03:36 +0200

    uses single quotes instead of double quotes

--- src/mw-ui-components/directives/mw_panel.js
+++ src/mw-ui-components/directives/mw_panel.js
@@ -5,9 +5,9 @@ angular.module('mwUI.UiComponents')
       restrict: 'A',
       transclude: true,
       scope: {
-        type: "@mwPanel",
-        title: "@",
-        closeable: "="
+        type: '@mwPanel',
+        title: '@',
+        closeable: '='
       },
       templateUrl: 'uikit/mw-ui-components/directives/templates/mw_panel.html',
       link: function(scope, el){
