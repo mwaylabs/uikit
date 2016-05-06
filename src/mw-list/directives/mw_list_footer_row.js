d84aff4fb6ecdf04239804784599dd93ac9d6b91
Zarlex <alex.zarges@online.de>
2016-05-06 15:03:25 +0200

    removes Loading dependency from relution portal

--- src/mw-list/directives/mw_list_footer_row.js
+++ src/mw-list/directives/mw_list_footer_row.js
@@ -1,13 +1,16 @@
 angular.module('mwUI.List')
   //TODO rename
-  .directive('mwListableFooterBb', function (Loading) {
+  .directive('mwListableFooterBb', function () {
     return {
       require: '^mwListableBb',
       templateUrl: 'uikit/mw-list/directives/templates/mw_list_footer.html',
       link: function (scope, elm, attr, mwListCtrl) {
-        scope.Loading = Loading;
         scope.collection = mwListCtrl.getCollection();
         scope.columns = mwListCtrl.getColumns();
+
+        scope.showSpinner = function(){
+          return /*Loading.isLoading() &&*/ scope.collection.filterable.hasNextPage();
+        }
       }
     };
   });
