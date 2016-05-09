a60c02efe7a8d26ba36ff157193a1487fd768c77
Zarlex <alex.zarges@online.de>
2016-05-09 21:08:36 +0200

    adds old collapsable directive to support markdown for backwards compatibility

--- src-relution/mwComponents.js
+++ src-relution/mwComponents.js
@@ -1,6 +1,62 @@
 'use strict';
 
 angular.module('mwComponents', [])
+
+  .directive('mwTextCollapse', function ($filter) {
+    return {
+      restrict: 'A',
+      scope: {
+        mwTextCollapse: '@',
+        length: '=',
+        markdown: '='
+      },
+      templateUrl: 'uikit/templates/mwComponents/mwTextCollapse.html',
+      link: function (scope) {
+
+        // set default length
+        if (scope.length && typeof scope.length === 'number') {
+          scope.defaultLength = scope.length;
+        } else {
+          scope.defaultLength = 200;
+        }
+
+        // set start length for filter
+        scope.filterLength = scope.defaultLength;
+
+        // apply filter length to text
+        scope.text = function () {
+          return $filter('reduceStringTo')(
+            scope.mwTextCollapse, scope.filterLength
+          );
+        };
+
+        // show Button if text is longer than desired
+        scope.showButton = false;
+        if (scope.mwTextCollapse.length > scope.defaultLength) {
+          scope.showButton = true;
+        }
+
+        // set button to "show more" or "show less"
+        scope.showLessOrMore = function () {
+          if (scope.filterLength === scope.defaultLength) {
+            return 'common.showMore';
+          } else {
+            return 'common.showLess';
+          }
+        };
+
+        // collapse/expand text by setting filter length
+        scope.toggleLength = function () {
+          if (scope.filterLength === scope.defaultLength) {
+            delete scope.filterLength;
+          } else {
+            scope.filterLength = scope.defaultLength;
+          }
+        };
+      }
+    };
+  })
+
   /**
    * @ngdoc directive
    * @name mwComponents.directive:mwFilterableSearch
