b42df119b8794efbe7b5d59e957efa3b1a0a4298
Zarlex <alex.zarges@online.de>
2016-06-03 11:17:15 +0200

    removes `use strict`

--- src-relution/mwFileUpload/mwFileUpload.js
+++ src-relution/mwFileUpload/mwFileUpload.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('mwFileUpload', [])
 
   .provider('mwFileUpload', function(){
--- src-relution/mwFileUpload/mwMimeType.js
+++ src-relution/mwFileUpload/mwMimeType.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('mwFileUpload')
 
     .service('mwMimetype', function () {
