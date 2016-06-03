391a253ab6fe0702475a28f082d92a3736d7aa1a
Zarlex <alex.zarges@online.de>
2016-06-03 11:00:19 +0200

    moves backbone config into run block to use angular $http and adds functionality to disable the use of $http and use backbone ajax instead

--- src/mw-backbone/mw_backbone_config.js
+++ src/mw-backbone/mw_backbone_config.js
@@ -1,28 +1,24 @@
-var _$http,
-  _$q,
-  _sync = Backbone.sync,
-  _ajax = Backbone.ajax;
-
 angular.module('mwUI.Backbone')
 
-  .run(function ($http, $q) {
-    _$http = $http;
-    _$q = $q;
-  });
+  .run(function ($http) {
+    var _backboneAjax = Backbone.ajax,
+      _backboneSync = Backbone.sync;
 
-Backbone.sync = function (method, model, options) {
-  // Instead of the response object we are returning the backbone model in the promise
-  return _sync.call(Backbone, method, model, options).then(function () {
-    return model;
+    Backbone.ajax = function (options) {
+      if (mwUI.Backbone.use$http) {
+        // Set HTTP Verb as 'method'
+        options.method = options.type;
+        // Use angulars $http implementation for requests
+        return $http.apply(angular, arguments);
+      } else {
+        return _backboneAjax.apply(this, arguments);
+      }
+    };
+
+    Backbone.sync = function (method, model, options) {
+      // Instead of the response object we are returning the backbone model in the promise
+      return _backboneSync.call(Backbone, method, model, options).then(function () {
+        return model;
+      });
+    };
   });
-};
-Backbone.ajax = function (options) {
-  if (_$http) {
-    // Set HTTP Verb as 'method'
-    options.method = options.type;
-    // Use angulars $http implementation for requests
-    return _$http.apply(angular, arguments);
-  } else {
-    return _ajax.apply(this, arguments);
-  }
-};
