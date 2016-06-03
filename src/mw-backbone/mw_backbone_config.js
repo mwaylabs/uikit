4693b32052ce0338885a255218943f64251c8216
Zarlex <alex.zarges@online.de>
2016-06-03 14:47:45 +0200

    moves backbone method overwriting out of run initialisation block because for some reasons it does not work

--- src/mw-backbone/mw_backbone_config.js
+++ src/mw-backbone/mw_backbone_config.js
@@ -1,24 +1,27 @@
+var _backboneAjax = Backbone.ajax,
+  _backboneSync = Backbone.sync,
+  _$http;
+
 angular.module('mwUI.Backbone')
 
   .run(function ($http) {
-    var _backboneAjax = Backbone.ajax,
-      _backboneSync = Backbone.sync;
-
-    Backbone.ajax = function (options) {
-      if (mwUI.Backbone.use$http) {
-        // Set HTTP Verb as 'method'
-        options.method = options.type;
-        // Use angulars $http implementation for requests
-        return $http.apply(angular, arguments);
-      } else {
-        return _backboneAjax.apply(this, arguments);
-      }
-    };
-
-    Backbone.sync = function (method, model, options) {
-      // Instead of the response object we are returning the backbone model in the promise
-      return _backboneSync.call(Backbone, method, model, options).then(function () {
-        return model;
-      });
-    };
+    _$http = $http;
   });
+
+Backbone.ajax = function (options) {
+  if (mwUI.Backbone.use$http && _$http) {
+    // Set HTTP Verb as 'method'
+    options.method = options.type;
+    // Use angulars $http implementation for requests
+    return _$http.apply(angular, arguments);
+  } else {
+    return _backboneAjax.apply(this, arguments);
+  }
+};
+
+Backbone.sync = function (method, model, options) {
+  // Instead of the response object we are returning the backbone model in the promise
+  return _backboneSync.call(Backbone, method, model, options).then(function () {
+    return model;
+  });
+};
