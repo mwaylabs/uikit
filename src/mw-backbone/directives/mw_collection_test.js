274e605ca2ecb763760b86329d3b44fce0a9ca4e
Zarlex <alex.zarges@online.de>
2016-06-20 20:27:52 +0200

    fixes test cases

--- src/mw-backbone/directives/mw_collection_test.js
+++ src/mw-backbone/directives/mw_collection_test.js
@@ -6,32 +6,27 @@ describe('testing mwModel', function () {
     this.$rootScope = $rootScope;
     this.scope = $rootScope.$new();
     this.$compile = $compile;
-    this.testCollection = new mwUI.Backbone.Collection()
-  }));
-
-  it('sets ng-model to value of mw-collection when at least one model is in collection', function () {
+    this.testCollection = new mwUI.Backbone.Collection();
     var input = '<input type="text" ng-model="text" mw-collection="testCollection"/>';
     this.$compile(input)(this.scope);
     this.scope.testCollection = this.testCollection;
+  }));
+
+  it('sets ng-model to value of mw-collection when at least one model is in collection', function () {
     this.testCollection.add({id:1});
+
     this.scope.$digest();
 
     expect(this.scope.text).toEqual(this.testCollection);
   });
 
   it('does not set ng-model to value of mw-collection when no model is in collection', function () {
-    var input = '<input type="text" ng-model="text" mw-collection="testCollection"/>';
-    this.$compile(input)(this.scope);
-    this.scope.testCollection = this.testCollection;
     this.scope.$digest();
 
-    expect(this.scope.text).toBeUndefined();
+    expect(this.scope.text).toBeNull();
   });
 
   it('sets ng-model to value of mw-collection when a model is added to collection', function () {
-    var input = '<input type="text" ng-model="text" mw-collection="testCollection"/>';
-    this.$compile(input)(this.scope);
-    this.scope.testCollection = this.testCollection;
     this.scope.$digest();
 
     this.testCollection.add({id: 1});
@@ -41,16 +36,13 @@ describe('testing mwModel', function () {
   });
 
   it('sets ng-model to value of mw-collection when a model is added to collection', function () {
-    var input = '<input type="text" ng-model="text" mw-collection="testCollection"/>';
-    this.$compile(input)(this.scope);
-    this.scope.testCollection = this.testCollection;
     this.testCollection.add({id: 1});
     this.scope.$digest();
 
     this.testCollection.remove({id: 1});
     this.scope.$digest();
 
-    expect(this.scope.text).toBeUndefined();
+    expect(this.scope.text).toBeNull();
   });
 
 
