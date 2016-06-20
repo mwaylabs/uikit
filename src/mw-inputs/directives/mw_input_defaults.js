3d328d9b8d6a4bf576473ae57c14ca4f858b0120
Zarlex <alex.zarges@online.de>
2016-06-20 20:25:08 +0200

    adds input type as class

--- src/mw-inputs/directives/mw_input_defaults.js
+++ src/mw-inputs/directives/mw_input_defaults.js
@@ -9,6 +9,11 @@ var extendInput = function () {
         if(skipTypes.indexOf(attrs.type)===-1){
           el.addClass('form-control');
         }
+        if(attrs.type){
+          mwInputWrapperCtrl.setType(attrs.type);
+        } else if(el[0].tagName){
+          mwInputWrapperCtrl.setType(el[0].tagName.toLowerCase());
+        }
       }
     }
   };
