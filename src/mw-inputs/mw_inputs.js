6b778a43dd4c4600e77c4a2ddcd2b5f23d337772
Zarlex <alex.zarges@online.de>
2016-06-23 17:22:47 +0200

    adds select box directive

--- src/mw-inputs/mw_inputs.js
+++ src/mw-inputs/mw_inputs.js
@@ -6,4 +6,10 @@ angular.module('mwUI.Inputs', ['mwUI.i18n', 'mwUI.Backbone']);
 // @include ./directives/mw_radio.js
 // @include ./directives/mw_radio_group.js
 // @include ./directives/mw_select.js
+// @include ./directives/mw_select_box.js
 // @include ./directives/mw_toggle.js
+
+angular.module('mwUI.Inputs')
+  .config(function(i18nProvider) {
+    i18nProvider.addResource('uikit/mw-inputs/i18n');
+  });
