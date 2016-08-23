ea647bcc42a5fdccc99a3c8be4dc14013dc4729d
Zarlex <alex.zarges@online.de>
2016-08-23 16:27:27 +0200

    refactors mw-infinite-scroll and fixes that the pagination inside the modal did not work
    can't provide a test that proof that fix because this can not be done by a unit test

--- src/mw-utils/directives/mw_infinite_scroll.js
+++ src/mw-utils/directives/mw_infinite_scroll.js
@@ -1,6 +1,6 @@
 angular.module('mwUI.Utils')
 
-  .directive('mwInfiniteScroll', function ($window, $document) {
+  .directive('mwInfiniteScroll', function ($window) {
     return {
       restrict: 'A',
       link: function (scope, el, attrs) {
@@ -8,9 +8,9 @@ angular.module('mwUI.Utils')
         var collection,
           loading = false,
           throttledScrollFn,
-          scrollFn,
-          documentEl,
-          scrollEl;
+          scrollContainerEl,
+          scrollContentEl,
+          loadedPages = 0;
 
         if (attrs.mwListCollection) {
           collection = scope.$eval(attrs.mwListCollection).getCollection();
@@ -24,48 +24,80 @@ angular.module('mwUI.Utils')
           return;
         }
 
-        var scrollCallback = function () {
-          if (!loading && scrollEl.scrollTop() >= ((documentEl.height() - scrollEl.height()) - 100) && collection.filterable.hasNextPage()) {
+        var loadNextPage = function () {
+          if (!loading && collection.filterable.hasNextPage()) {
             loading = true;
-            collection.filterable.loadNextPage().then(function () {
-              loading = false;
-            });
-          }
-        };
-        var modalScrollCallback = function () {
-          if (!loading &&
-            collection.filterable.hasNextPage() &&
-            scrollEl[0].scrollHeight > 0 &&
-            (scrollEl[0].scrollHeight - scrollEl.scrollTop() - scrollEl[0].clientHeight < 2)) {
-            loading = true;
-            collection.filterable.loadNextPage().then(function () {
+            loadedPages++;
+            return collection.filterable.loadNextPage().then(function () {
               loading = false;
             });
           }
         };
 
-        if (el.parents('.modal').length) {
+        // The threshold is controlled by how many pages are loaded
+        // The first pagination request should be done quiet early so the user does not recognize that something
+        // is loaded.
+        // As scrollbar is getting longer and longer the threshold has to be increased as well.
+        // Threshold starts at 40% and is increased by 10% until the max threshold of 90% is reached
+        var getLoadNextThreshold = function(){
+          var minThreshold = 4,
+              maxThreshold = 9;
+
+          return Math.min(minThreshold+loadedPages,maxThreshold)/10;
+        };
+
+        var scrollFn = function () {
+          var contentHeight = scrollContentEl[0].clientHeight || scrollContentEl.height(),
+              totalHeight = contentHeight - scrollContainerEl.height(),
+              threshold = getLoadNextThreshold();
+
+          if ( scrollContainerEl.scrollTop() / totalHeight > threshold) {
+            loadNextPage();
+          }
+        };
+
+        if(attrs.scrollContainerSelector || attrs.scrollContentSelector){
+          // Custom element defined by selectors. The scrollContainer is the element that has the scrollbar
+          // The scrollContent is the element inside the scroll container that should be scrolled
+          // At least one of them has to be defined and if you define both they must not be the same
+          if(attrs.scrollContainerSelector && el.parents(attrs.scrollContainerSelector).length > 0){
+            scrollContainerEl = el.parents(attrs.scrollContainerSelector).first();
+          } else if(attrs.scrollContainerSelector && el.parents(attrs.scrollContainerSelector).length===0){
+            throw new Error ('No parent of the infinite scroll element with the selector '+attrs.scrollContainerSelector+' could be found!');
+          } else {
+            scrollContainerEl = el;
+          }
+
+          if(attrs.scrollContentSelector && el.find(attrs.scrollContentSelector).length > 0){
+            scrollContentEl = el.find(attrs.scrollContentSelector).first();
+          } else if(attrs.scrollContentSelector && el.find(attrs.scrollContentSelector).length === 0){
+            throw new Error ('No child of the infinite scroll element with the selector '+attrs.scrollContentSelector+' could be found!');
+          } else {
+            scrollContentEl = el;
+          }
+        } else if (el.parents('.modal').length) {
           //element in modal
-          scrollEl = el.parents('.modal-body');
-          scrollFn = modalScrollCallback;
-        }
-        else {
+          scrollContainerEl = el.parents('*[mw-modal-body]');
+          scrollContentEl = el.parents('.modal-body');
+        } else {
           //element in window
-          documentEl = angular.element($document);
-          scrollEl = angular.element($window);
-          scrollFn = scrollCallback;
+          scrollContainerEl = angular.element($window);
+          scrollContentEl = angular.element(document);
+        }
+
+        if(scrollContainerEl === scrollContentEl){
+          throw new Error('The scrollContainerElement can not be the same as the actual scrollContentElement');
         }
 
         throttledScrollFn = _.throttle(scrollFn, 500);
 
         // Register scroll callback
-        scrollEl.on('scroll', throttledScrollFn);
+        scrollContainerEl.on('scroll', throttledScrollFn);
 
         // Deregister scroll callback if scope is destroyed
         scope.$on('$destroy', function () {
-          scrollEl.off('scroll', throttledScrollFn);
+          scrollContainerEl.off('scroll', throttledScrollFn);
         });
-
       }
     };
   });
---
+++ src/mw-utils/directives/mw_infinite_scroll_test.js
@@ -0,0 +1,152 @@
+describe('mwInfiniteScroll', function () {
+  beforeEach(module('karmaDirectiveTemplates'));
+  beforeEach(module('mwUI.Utils'));
+  beforeEach(module('mwUI.Modal'));
+
+  beforeEach(inject(function ($window, $q, $compile, $rootScope) {
+    this.$q = $q;
+    this.$compile = $compile;
+    this.$rootScope = $rootScope;
+    this.scope = $rootScope.$new();
+    this.collection = new mwUI.Backbone.Collection();
+    this.hasNextPageSpy = spyOn(this.collection.filterable, 'hasNextPage').and.callFake(function () {
+      return true;
+    });
+    this.loadNextSpy = spyOn(this.collection.filterable, 'loadNextPage').and.callFake(function () {
+      var dfd = $q.defer();
+      dfd.resolve();
+      return dfd.promise;
+    });
+  }));
+
+  describe('with custom scroll container', function () {
+    beforeEach(function () {
+      this.infiniteScrollTmpl =
+        '<div id="scrollContainer" style="height:2000px; overflow: scroll">' +
+        '<div mw-infinite-scroll ' +
+        'collection="collection" ' +
+        'style="height: 3000px" ' +
+        'scroll-container-selector="#scrollContainer">' +
+        '</div>' +
+        '</div>';
+      this.scope.collection = this.collection;
+      this.$el = this.$compile(this.infiniteScrollTmpl)(this.scope);
+      this.scope.$digest();
+      this.$bodyEl = angular.element('body');
+      this.$bodyEl.append(this.$el);
+      this.$scrollEl = this.$el;
+      jasmine.clock().install();
+
+      this.scrollToPos = function(pos, append){
+        var el = this.$el.find('*[mw-infinite-scroll]');
+        this.$scrollEl.scrollTop(pos);
+        this.$scrollEl.triggerHandler('scroll');
+        jasmine.clock().tick(501);
+        if(append!==false){
+          el.height(parseInt(el.height(),10)+3000);
+        }
+        this.scope.$digest();
+      };
+    });
+
+    afterEach(function () {
+      this.$el.remove();
+      jasmine.clock().uninstall();
+    });
+
+    it('paginates on scroll when filterable is available and has a next page', function () {
+      this.scrollToPos(3000);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalled();
+    });
+
+    it('does not paginate on scroll when filterable has no more pages', function () {
+      this.hasNextPageSpy.and.callFake(function () {
+        return false;
+      });
+
+      this.scrollToPos(3000);
+
+      expect(this.scope.collection.filterable.loadNextPage).not.toHaveBeenCalled();
+    });
+
+    it('does not trigger another load request when a load request is already pending', function () {
+      this.loadNextSpy.and.callFake(function () {
+        return this.$q.defer().promise;
+      }.bind(this));
+      this.scrollToPos(2999, false);
+
+      this.scrollToPos(3000);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(1);
+    });
+
+    it('does trigger another load request when the previous one is done and another scroll happens', function () {
+      var dfd = this.$q.defer();
+      this.loadNextSpy.and.callFake(function () {
+        return dfd.promise;
+      }.bind(this));
+      this.scrollToPos(2999, false);
+
+      dfd.resolve();
+      this.scope.$digest();
+      this.scrollToPos(3000);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(2);
+    });
+
+    it('has a minimum threshold of 40% scrolled before a load request is triggered', function () {
+      // scroll el has a height of 3000px, container 2000px, min threshold starts at 40%
+      // (3000px-20000px)*0.4 = 400px
+      this.scrollToPos(399, false);
+
+      this.scrollToPos(401);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(1);
+    });
+
+    it('triggers not another request before the next threshold stage is reached', function () {
+      // (3000px-20000px)*0.4 = 400px
+      this.scrollToPos(401);
+
+      // (6000px-20000px)*0.4 = 1600px
+      this.scrollToPos(1601);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(1);
+    });
+
+    it('increases threshold before it loads next page over time', function () {
+      // (3000px-20000px)*0.4 = 400px
+      this.scrollToPos(401);
+      // (6000px-20000px)*0.5 = 2000px
+      this.scrollToPos(2001);
+      // (9000px-20000px)*0.6 = 4200px
+      this.scrollToPos(4201);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(3);
+    });
+
+    it('has a maximum threshold of 90% after that it wont be increased anymore', function () {
+      this.scrollToPos(401);
+      this.scrollToPos(2001);
+      this.scrollToPos(4201);
+      this.scrollToPos(7001);
+      this.scrollToPos(10401);
+      // (18000px-20000px)*0.9 = 14401px
+      this.scrollToPos(14401);
+
+      // (21000px-20000px)*0.9 = 17100px
+      this.scrollToPos(17101);
+
+      expect(this.scope.collection.filterable.loadNextPage).toHaveBeenCalledTimes(7);
+    });
+  });
+
+  describe('in document', function () {
+    // Can not be tested because the phantomJs viewport does not have a real height -> has to be tested by a integration test like protractor
+  });
+
+  describe('in modal', function () {
+    // Can not be tested because stylesheet of modal is not available -> has to be tested by a integration test like protractor
+  });
+});
