a30d84b7b1e7bb6b9d0f6930f5bca902170636db
Bob Builder <info@mwaysolutions.com>
2016-10-13 18:30:53 +0200

    tmp release

--- dist/mw-uikit.js
+++ dist/mw-uikit.js
@@ -41,7 +41,7 @@
 
   //Will be replaced with the actual version number duringh the build process;
   //DO NOT TOUCH
-  root.mwUI.VERSION = '1.0.0-b33';
+  root.mwUI.VERSION = '1.0.0-b1';
 
 angular.module("mwUI").run(["$templateCache", function($templateCache) {  'use strict';
 
