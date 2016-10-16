5e271c28fcd1c7a262544a2140e9c52b5cda32a5
Bob Builder <info@mwaysolutions.com>
2016-10-16 18:05:03 +0200

    release version 1.0.1-b1234567

--- dist/dist/mw-uikit.js
+++ dist/dist/mw-uikit.js
@@ -41,7 +41,7 @@
 
   //Will be replaced with the actual version number duringh the build process;
   //DO NOT TOUCH
-  root.mwUI.VERSION = '1.0.1-b123456';
+  root.mwUI.VERSION = '1.0.1-b1234567';
 
 angular.module("mwUI").run(["$templateCache", function($templateCache) {  'use strict';
 
