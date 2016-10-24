b14ed6b9f8e1b286090ed3797207c79524708772
zarlex <alex.zarges@online.de>
2016-10-24 17:08:10 +0200

    fixes typo

--- src/mw-backbone/utils/concat_url_parts.js
+++ src/mw-backbone/utils/concat_url_parts.js
@@ -6,11 +6,11 @@ mwUI.Backbone.Utils.concatUrlParts = function () {
     if (!_.isString(url) || url === '') {
       return;
     } else if (index !== 0 || url === '/') {
-      //Removes slash at the beginning of the string except when it is only a slash or first word
+      //Removes slash at the beginning of the string except when it is the first argument or it is only a slash
       url = url.replace(/^\//, '');
     }
 
-    // Removes last slash
+    // Removes trailing slash
     url = url.replace(/\/$/, '');
 
     trimmedUrlParts.push(url);
