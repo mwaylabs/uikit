9d560353b3b52e7a6e5e02178f972e0dc6f964c6
zarlex <alex.zarges@online.de>
2016-11-04 12:18:32 +0100

    add missing semicolon to fix jshint linting error

--- src/mw-backbone/utils/get_url.js
+++ src/mw-backbone/utils/get_url.js
@@ -4,7 +4,7 @@ mwUI.Backbone.Utils.getUrl = function(instance){
   if(instance instanceof mwUI.Backbone.Model || instance instanceof mwUI.Backbone.Collection){
     hostName = _.result(instance, 'hostName') || '';
     basePath = _.result(instance, 'basePath') || '';
-    endpoint = _.result(instance, 'endpoint')
+    endpoint = _.result(instance, 'endpoint');
   } else {
     throw new Error('An instance of a collection or a model has to be passed as argument to the function');
   }
