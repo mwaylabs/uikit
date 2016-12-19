43856a57429ad0b141625db095d57601a2b9688a
zarlex <alex.zarges@online.de>
2016-12-19 14:21:53 +0100

    change menu entry that it just registers menu entry but has no display logic

--- src/mw-menu/directives/mw_menu_entry.js
+++ src/mw-menu/directives/mw_menu_entry.js
@@ -3,20 +3,69 @@
  */
 angular.module('mwUI.Menu')
 
-  .directive('mwMenuEntry', function () {
+  .directive('mwMenuEntry', function ($timeout) {
     return {
-      scope: {},
-      bindToController: {
-        entry: '=mwMenuEntry'
+      scope: {
+        id: '@',
+        url: '@',
+        icon: '@',
+        label: '@',
+        order: '='
       },
       templateUrl: 'uikit/mw-menu/directives/templates/mw_menu_entry.html',
       controllerAs: 'menuEntryCtrl',
-      controller: function($rootScope, $location){
-        this.isActive = this.entry.isActiveForUrl($location.url());
+      require: ['mwMenuEntry', '?^^mwMenuEntry', '?^mwMenuTop'],
+      transclude: true,
+      controller: function () {
+        var _menuEntry;
 
-        $rootScope.$on('$locationChangeSuccess', function () {
-          this.isActive = this.entry.hasActiveSubEntryOrIsActiveForUrl($location.url());
-        }.bind(this));
+        this.setMenuEntry = function (menuEntry) {
+          _menuEntry = menuEntry;
+        };
+
+        this.getMenuEntry = function () {
+          return _menuEntry;
+        };
+      },
+      link: function (scope, el, attrs, ctrls) {
+        var ctrl = ctrls[0],
+          parentCtrl = ctrls[1],
+          menuCtrl = ctrls[2],
+          menuEntry = new mwUI.Menu.MwMenuEntry({
+            id: scope.id || scope.url || scope.$id,
+            label: scope.label,
+            url: scope.url,
+            icon: scope.icon,
+            type: 'ENTRY',
+            order: scope.order || 0
+          }),
+          entryHolder;
+
+        scope.menuEntry = menuEntry;
+
+        $timeout(function(){
+          if (parentCtrl) {
+            entryHolder = parentCtrl.getMenuEntry().get('subEntries');
+          } else if (menuCtrl) {
+            entryHolder = menuCtrl.getMenu();
+          }
+
+          if(entryHolder){
+            if(entryHolder.get(menuEntry)){
+              menuEntry.show();
+            } else {
+              entryHolder.add(menuEntry);
+            }
+          }
+        });
+
+        ctrl.setMenuEntry(menuEntry);
+
+        scope.$on('$destroy', function(){
+          if(entryHolder){
+            entryHolder.remove(menuEntry);
+          }
+        });
       }
     };
   });
