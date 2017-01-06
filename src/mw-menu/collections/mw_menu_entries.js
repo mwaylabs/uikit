915aeae1d8df70d11aa4c45560c0c07742b81659
zarlex <alex.zarges@online.de>
2017-01-06 14:36:17 +0100

    add set default order functionality

--- src/mw-menu/collections/mw_menu_entries.js
+++ src/mw-menu/collections/mw_menu_entries.js
@@ -4,6 +4,18 @@
 var MwMenuEntries = Backbone.Collection.extend({
   model: window.mwUI.Menu.MwMenuEntry,
   comparator: 'order',
+  _currentOrder: 0,
+  _setDefaultOrder: function(entry) {
+    if (entry instanceof Backbone.Model) {
+      if (!entry.get('order')) {
+        entry.set('order', this._currentOrder++);
+      }
+    } else if (_.isObject(entry)) {
+      if (!entry.order) {
+        entry.order = this._currentOrder++;
+      }
+    }
+  },
   _isAlreadyRegistered: function(entry) {
     return (
       this.get(entry.id) ||
@@ -22,17 +34,23 @@ var MwMenuEntries = Backbone.Collection.extend({
       entries.forEach(function(entry){
         if(this._isAlreadyRegistered(entry)){
           this._throwIsAlreadyRegisteredError(entry);
+          this._setDefaultOrder(entry);
         }
+        this._setDefaultOrder(entry);
       }.bind(this));
     } else {
       if(this._isAlreadyRegistered(entries)){
         this._throwIsAlreadyRegisteredError(entries);
       }
+      this._setDefaultOrder(entries);
     }
     return Backbone.Collection.prototype.add.apply(this, arguments);
   },
   addEntry: function(id, url, label, options){
     options = options || {};
+    if(!options.order){
+      options.order = this._currentOrder++;
+    }
     var addObj = {
       id: id,
       url: url,
