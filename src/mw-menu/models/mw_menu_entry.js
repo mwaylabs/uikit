cc91940dc0bb26d602cdd842219c272d8f531361
zarlex <alex.zarges@online.de>
2017-01-06 20:03:19 +0100

    intend code and move validity check into validity function

--- src/mw-menu/models/mw_menu_entry.js
+++ src/mw-menu/models/mw_menu_entry.js
@@ -2,7 +2,7 @@ var routeToRegex = mwUI.Utils.shims.routeToRegExp;
 
 var MwMenuEntry = window.mwUI.Backbone.NestedModel.extend({
   idAttribute: 'id',
-  defaults: function(){
+  defaults: function () {
     return {
       url: null,
       label: null,
@@ -12,26 +12,26 @@ var MwMenuEntry = window.mwUI.Backbone.NestedModel.extend({
       isVisible: true
     };
   },
-  nested: function(){
+  nested: function () {
     return {
       subEntries: window.mwUI.Menu.MwMenuSubEntries
     };
   },
-  _throwMissingIdError: function(entry){
+  _throwMissingIdError: function (entry) {
     throw new Error('No id is specified for the entry', entry);
   },
-  _throwNoTypeCouldBeDeterminedError: function(entry){
-    throw new Error('No type could be determinded for the given entry: ',entry);
+  _throwNoTypeCouldBeDeterminedError: function (entry) {
+    throw new Error('No type could be determinded for the given entry: ', entry);
   },
-  _throwNotValidEntryError: function(entry){
+  _throwNotValidEntryError: function (entry) {
     throw new Error('Is not a valid entry', entry);
   },
-  _determineType: function(entry){
-    if(!entry.type){
-      if(!entry.url && (!entry.subEntries || entry.subEntries.length===0) && !(entry.label || entry.icon)){
-        entry.type='DIVIDER';
-      } else if(entry.url || entry.subEntries && entry.subEntries.length>0 && (entry.label || entry.icon)){
-        entry.type='ENTRY';
+  _determineType: function (entry) {
+    if (!entry.type) {
+      if (!entry.url && (!entry.subEntries || entry.subEntries.length === 0) && !(entry.label || entry.icon)) {
+        entry.type = 'DIVIDER';
+      } else if (entry.url || entry.subEntries && entry.subEntries.length > 0 && (entry.label || entry.icon)) {
+        entry.type = 'ENTRY';
       } else {
         this._throwNoTypeCouldBeDeterminedError();
       }
@@ -39,71 +39,84 @@ var MwMenuEntry = window.mwUI.Backbone.NestedModel.extend({
 
     return entry;
   },
-  _missingLabel: function(entry){
+  _missingLabel: function (entry) {
     return entry.type === 'ENTRY' && !(entry.label || !entry.icon);
   },
-  _urlsAreMatching: function(url, matchUrl){
-    if(matchUrl.match('#')){
+  _urlsAreMatching: function (url, matchUrl) {
+    if (matchUrl.match('#')) {
       matchUrl = matchUrl.split('#')[1];
     }
     return url.match(routeToRegex(matchUrl));
   },
-  show: function(){
-    this.set('isVisible',true);
+  validate: function (entry) {
+    if (entry && _.isObject(entry)) {
+      entry = this._determineType(entry);
+      if (!entry.id) {
+        this._throwMissingIdError();
+      }
+      if (!this.isValidEntry(entry)) {
+        this._throwNotValidEntryError(entry);
+      }
+    }
   },
-  hide: function(){
-    this.set('isVisible',false);
+  set: function (entry, options) {
+    options = options || {};
+    if (_.isUndefined(options.validate)) {
+      this.validate(entry);
+    }
+    return window.mwUI.Backbone.NestedModel.prototype.set.call(this, entry, options);
   },
-  isValidEntry: function(entry){
-    if(entry.type){
+  show: function () {
+    this.set('isVisible', true);
+  },
+  hide: function () {
+    this.set('isVisible', false);
+  },
+  isValidEntry: function (entry) {
+    if (entry.type) {
       return !this._missingLabel(entry);
     } else {
       return false;
     }
   },
-  ownUrlIsActiveForUrl: function(url){
-    if(this.get('url')){
+  ownUrlIsActiveForUrl: function (url) {
+    if (this.get('url')) {
       return this._urlsAreMatching(url, this.get('url'));
     } else {
       return false;
     }
   },
-  activeUrlIsActiveForUrl: function(url){
+  activeUrlIsActiveForUrl: function (url) {
     var isActive = false;
-    this.get('activeUrls').forEach(function(activeUrl){
-      if(!isActive){
-        isActive = this._urlsAreMatching(url,activeUrl);
+    this.get('activeUrls').forEach(function (activeUrl) {
+      if (!isActive) {
+        isActive = this._urlsAreMatching(url, activeUrl);
       }
     }.bind(this));
     return isActive;
   },
-  isSubEntry: function(){
-    if(this.collection && this.collection.parent){
+  isSubEntry: function () {
+    if (this.collection && this.collection.parent) {
       return true;
     }
     return false;
   },
-  hasSubEntries: function(){
-    return this.get('subEntries').length>0;
+  hasSubEntries: function () {
+    return this.get('subEntries').length > 0;
   },
-  isActiveForUrl: function(url){
+  isActiveForUrl: function (url) {
     return this.ownUrlIsActiveForUrl(url) || this.activeUrlIsActiveForUrl(url);
   },
-  getActiveSubEntryForUrl: function(url){
+  getActiveSubEntryForUrl: function (url) {
     return this.get('subEntries').getActiveEntryForUrl(url);
   },
-  hasActiveSubEntryOrIsActiveForUrl: function(url){
+  hasActiveSubEntryOrIsActiveForUrl: function (url) {
     return this.get('type') === 'ENTRY' && (!!this.getActiveSubEntryForUrl(url) || this.isActiveForUrl(url));
   },
-  constructor: function(entry, options){
-    entry = this._determineType(entry);
-    if(!entry.id){
-      this._throwMissingIdError();
-    }
-    if(!this.isValidEntry(entry)){
-      this._throwNotValidEntryError(entry);
-    }
-    return window.mwUI.Backbone.NestedModel.prototype.constructor.call(this, entry, options);
+  constructor: function (model, options) {
+    options = options || {};
+    options.validate = model ? true : false;
+    return window.mwUI.Backbone.NestedModel.prototype.constructor.call(this, model, options);
   }
 });
 
