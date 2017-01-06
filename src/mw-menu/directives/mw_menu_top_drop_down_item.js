391240a764a5a7500e629706a235e300cbc6999b
zarlex <alex.zarges@online.de>
2017-01-06 14:35:51 +0100

    remove navbar collapse

--- src/mw-menu/directives/mw_menu_top_drop_down_item.js
+++ src/mw-menu/directives/mw_menu_top_drop_down_item.js
@@ -5,11 +5,6 @@ angular.module('mwUI.Menu')
       scope: {
         entry: '=mwMenuTopDropDownItem'
       },
-      templateUrl: 'uikit/mw-menu/directives/templates/mw_menu_top_drop_down_item.html',
-      link: function(scope, el){
-        el.find('a').on('click', function() {
-          scope.$emit('nav:uncollapse');
-        });
-      }
+      templateUrl: 'uikit/mw-menu/directives/templates/mw_menu_top_drop_down_item.html'
     };
   });
