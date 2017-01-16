cde97d0221fdff3a4329202b62f1aafe0bfd6e09
zarlex <alex.zarges@online.de>
2017-01-16 15:26:46 +0100

    register/unregister link to action colums with id and update it when link is changing

--- src/mw-list/directives/mw_list_url_action_button.js
+++ src/mw-list/directives/mw_list_url_action_button.js
@@ -4,13 +4,35 @@ angular.module('mwUI.List')
     return {
       restrict: 'A',
       require: '^mwListableBb',
-      scope: {
-        link: '@mwListableLinkShowBb',
-        target: '@?'
-      },
-      template: '<span mw-link-show="{{link}}" link-target="{{target}}"></span>',
+      template: '<span mw-link-show="{{link}}" link-target="{{linkTarget}}"></span>',
       link: function (scope, elm, attr, mwListableCtrl) {
-        mwListableCtrl.actionColumns.push(scope.link);
+        var updateLink = function(link){
+          if(_.isNumber(scope.$index) && link){
+            var existingItem = _.findWhere(mwListableCtrl.actionColumns, {id: scope.$index});
+
+            if(existingItem){
+              existingItem.link = link;
+            } else {
+              mwListableCtrl.actionColumns.push({id: scope.$index, link: link});
+            }
+          }
+          scope.link = link;
+        };
+
+        var removeLink = function(){
+          if(scope.$index){
+            var existingItem = _.findWhere(mwListableCtrl.actionColumns, {id:scope.$index});
+            if(existingItem){
+              var indexOfExistingItem = _.indexOf(mwListableCtrl.actionColumns, existingItem);
+              mwListableCtrl.actionColumns.splice(indexOfExistingItem, 1)
+            }
+          }
+        };
+
+        scope.linkTarget = attr.linkTarget;
+        updateLink(attr.mwListableLinkShowBb);
+        attr.$observe('mwListableLinkShowBb', updateLink);
+        scope.$on('$destroy', removeLink);
       }
     };
   });
