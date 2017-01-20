35f411af1ac82296b19168cec67a84099024b818
zarlex <alex.zarges@online.de>
2017-01-20 18:03:41 +0100

    rename to mw_menu_top_entries

--- src/mw-menu/mw_menu.js
+++ src/mw-menu/mw_menu.js
@@ -11,8 +11,8 @@ window.mwUI.Menu = {};
 // @include ./collections/mw_menu_sub_entries.js
 // @include ./collections/mw_menu.js
 
-// @include ./directives/mw_menu_top.js
 // @include ./directives/mw_menu_top_bar.js
+// @include ./directives/mw_menu_top_entries.js
 // @include ./directives/mw_menu_top_drop_down_item.js
 // @include ./directives/mw_menu_top_item.js
 // @include ./directives/mw_menu_entry.js
