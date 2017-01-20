4b9d01bbb95e3b6e6b5d483359c6507c4790c4e4
zarlex <alex.zarges@online.de>
2017-01-20 21:03:12 +0100

    use multi transclude to transclude brand logo and menu entries

--- src/mw-menu/directives/mw_menu_top_bar.js
+++ src/mw-menu/directives/mw_menu_top_bar.js
@@ -2,10 +2,10 @@ angular.module('mwUI.Menu')
 
   .directive('mwMenuTopBar', function () {
     return {
-      scope: {
-        logo: '@'
+      transclude: {
+        'brand': '?img',
+        'entries': '?div'
       },
-      transclude: true,
       templateUrl: 'uikit/mw-menu/directives/templates/mw_menu_top_bar.html'
     };
   });
