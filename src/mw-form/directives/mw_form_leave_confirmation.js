220cd206af4c107b1f762eda3e4f0a8ed3b2e70a
zarlex <alex.zarges@online.de>
2017-02-14 19:55:12 +0100

    fix missing translation and and call function instead of from $dirty check

--- src/mw-form/directives/mw_form_leave_confirmation.js
+++ src/mw-form/directives/mw_form_leave_confirmation.js
@@ -1,13 +1,13 @@
 angular.module('mwUI.Form')
 
-  .directive('mwFormLeaveConfirmation', function ($window, $document, $location, i18n, Modal, $compile) {
+  .directive('mwFormLeaveConfirmation', function ($compile) {
     return {
       require: '^form',
       link: function (scope, elm, attr, formCtrl) {
 
         var confirmation = $compile('' +
-            '<div mw-leave-confirmation="form.$dirty" ' +
-            'text="{{\'Form.mwFormLeaveConfirmation.isDirty\' | i18n}}">' +
+            '<div mw-leave-confirmation="showConfirmation()" ' +
+            'text="{{\'Form.leaveConfirmation\' | i18n}}">' +
             '</div>')(scope),
           isActive = true;
 
