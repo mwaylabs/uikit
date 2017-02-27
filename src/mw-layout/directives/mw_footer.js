77d935e8e1f8be9208aef9b4ff3ff505cba97990
zarlex <alex.zarges@online.de>
2017-02-27 18:37:45 +0100

    add footer directive

---
+++ src/mw-layout/directives/mw_footer.js
@@ -0,0 +1,12 @@
+angular.module('mwUI.Layout')
+
+  .directive('mwFooter', function () {
+    return {
+      transclude: true,
+      scope: {
+        type: '@mwFooter'
+      },
+      templateUrl: 'uikit/mw-layout/directives/templates/mw_footer.html',
+      link: function (scope, el) {}
+    };
+  });
