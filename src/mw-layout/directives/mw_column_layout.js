177d3cc3e8ebf8f6d1f2ad73b56d270c118bec78
zarlex <alex.zarges@online.de>
2017-02-27 18:10:32 +0100

    remove unused directives

--- src/mw-layout/directives/mw_column_layout.js
+++
@@ -1,9 +0,0 @@
-angular.module('mwUI.Layout')
-
-  .directive('mwColumnLayout', function () {
-    return {
-      link: function(scope, el){
-        el.addClass('mw-column-layout');
-      }
-    };
-  });
