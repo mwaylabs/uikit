bd9a9b9ad25f1a0a188ba508f855472f2b50ea4c
zarlex <alex.zarges@online.de>
2017-07-07 11:09:51 +0200

    Remove unused dependency

--- src-relution/mwListCollectionFilter.js
+++ src-relution/mwListCollectionFilter.js
@@ -4,7 +4,6 @@ angular.module('mwCollection')
                                                $rootScope,
                                                $location,
                                                $route,
-                                               $timeout,
                                                LocalForage,
                                                FilterHoldersCollection,
                                                FilterHolderProvider) {
