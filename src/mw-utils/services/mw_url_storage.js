321b2e3a8caa0893d956767c0cd16d3ae32e5e25
zarlex <alex.zarges@online.de>
2017-07-21 19:09:46 +0200

    adds url storage filter to store attributes as query params

---
+++ src/mw-utils/services/mw_url_storage.js
@@ -0,0 +1,116 @@
+angular.module('mwUI.Utils')
+
+  .service('mwUrlStorage', function ($rootScope, $location, $route) {
+    var storage = {};
+
+    var preventRouteReload = function () {
+      //Check whether reloadOnSearch is already disabled
+      if ($route.current.$$route.reloadOnSearch === false) {
+        return;
+      }
+      // Remember the state so we can set it to the original state after we have updated the route
+      var prevReloadOnSearchVal = $route.current.$$route.reloadOnSearch;
+      //Set reloadOnSearch false so angular does not reinitialize the controller
+      $route.current.$$route.reloadOnSearch = false;
+      //Route update is triggered when reloadOnSearch is set to true and a search param has changed
+      var unbindRouteUpdateListener = $rootScope.$on('$routeUpdate', function () {
+        $route.current.$$route.reloadOnSearch = prevReloadOnSearchVal;
+        unbindRouteUpdateListener();
+        unbindRouteChangeSuccessListener();
+      });
+      // //Route change success is triggered when reloadOnSearch is set to false and a search param has changed
+      var unbindRouteChangeSuccessListener = $rootScope.$on('$routeChangeSuccess', function () {
+        $route.current.$$route.reloadOnSearch = prevReloadOnSearchVal;
+        unbindRouteChangeSuccessListener();
+        unbindRouteUpdateListener();
+      });
+    };
+
+    var getChangedValues = function (params) {
+      var currentSearchParams = $location.search();
+      return _.difference(_.values(params), _.values(currentSearchParams));
+    };
+
+    var setUrlQueryParams = function (params) {
+      if (getChangedValues(params).length > 0) {
+        var currentSearchParams = $location.search(),
+          newSearchParams = _.extend(currentSearchParams, params);
+
+        preventRouteReload();
+        $location.search(newSearchParams);
+      }
+    };
+
+    $rootScope.$on('$locationChangeSuccess', function (ev, newUrl, oldUrl) {
+      if (newUrl !== oldUrl) {
+        setUrlQueryParams(storage);
+      }
+    });
+
+    return {
+      getItem: function (key) {
+        return $location.search()[key];
+      },
+      setObject: function (obj, options) {
+        options = options || {};
+        var wasChanged = false;
+        if (_.isObject(obj)) {
+          for (var key in obj) {
+            if (obj.hasOwnProperty(key) && storage[key] !== obj[key]) {
+              if (!options.removeOnUrlChange) {
+                storage[key] = obj[key];
+              }
+              wasChanged = true;
+            }
+          }
+        } else {
+          throw new Error('[mwUrlStorage] parameter has to be an object otherwise setItem(key, val) should be called ');
+        }
+
+        if (wasChanged) {
+          setUrlQueryParams(obj);
+        }
+      },
+      setItem: function (key, value, options) {
+        options = options || {};
+        if (storage[key] !== value) {
+          if (!options.removeOnUrlChange) {
+            storage[key] = value;
+          }
+          var obj = {};
+          obj[key] = value;
+          setUrlQueryParams(obj);
+        }
+      },
+      removeObject: function () {
+        var wasChanged = false;
+        if (_.isObject(obj)) {
+          for (var key in obj) {
+            if (obj.hasOwnProperty(key) && storage[key]) {
+              storage[key] = null;
+              wasChanged = true;
+            }
+          }
+        } else {
+          throw new Error('[mwUrlStorage] parameter has to be an object otherwise deleteItem(key, val) should be called ');
+        }
+
+        if (wasChanged) {
+          setUrlQueryParams(storage);
+        }
+      },
+      removeItem: function (key) {
+        if (storage[key]) {
+          storage[key] = null;
+          setUrlQueryParams(storage);
+          return true;
+        } else {
+          return false;
+        }
+      },
+      clear: function () {
+        storage = {};
+        setUrlQueryParams(storage);
+      }
+    };
+  });
---
+++ src/mw-utils/services/mw_url_storage_test.js
@@ -0,0 +1,108 @@
+'use strict';
+
+fdescribe('MwUrlStorageTest', function() {
+  var queryParams = {},
+      currentUrl = '',
+      $rootScope,
+      $q,
+      subject,
+      routeSpy,
+      locationSpy;
+
+  beforeEach(module('mwUI.Utils'));
+
+  beforeEach(module(function($provide) {
+    locationSpy = {
+      url: function() {
+        return currentUrl;
+      },
+      path: function(newPath) {
+        this.search({});
+        $rootScope.$broadcast('$locationChangeStart');
+        $rootScope.$broadcast('$routeChangeStart');
+        $rootScope.$broadcast('$locationChangeSuccess', null, newPath, currentUrl);
+        $rootScope.$broadcast('$routeChangeSuccess');
+        currentUrl = newPath;
+        return locationSpy;
+      },
+      search: function(params) {
+        if(params && _.isObject(params)){
+          queryParams = params;
+        }
+        return queryParams;
+      }
+    };
+    spyOn(locationSpy, 'path').and.callThrough();
+    spyOn(locationSpy, 'search').and.callThrough();
+    spyOn(locationSpy, 'url').and.callThrough();
+    $provide.value('$location', locationSpy);
+
+    routeSpy = {
+      current: {
+        $$route: {}
+      }
+    };
+    $provide.value('$route', routeSpy);
+  }));
+
+  beforeEach(inject(function(_$rootScope_, _$q_, mwUrlStorage) {
+    $rootScope = _$rootScope_;
+    $q = _$q_;
+    subject = mwUrlStorage;
+  }));
+
+  afterEach(function(){
+    this.queryParams = {};
+    this.currentUrl = '';
+  });
+
+  it('sets query param when calling setItem', function() {
+    subject.setItem('abc','IRRELEVANT');
+
+    expect(locationSpy.search).toHaveBeenCalledWith({abc: 'IRRELEVANT'});
+  });
+
+  it('updated query param when calling setItem and a previous value does exist', function() {
+    subject.setItem('abc','IRRELEVANT');
+
+    subject.setItem('abc','IRRELEVANT_UPDATE');
+
+    expect(locationSpy.search).toHaveBeenCalledWith({abc: 'IRRELEVANT_UPDATE'});
+  });
+
+  it('removes query param when calling removeItem', function() {
+    subject.setItem('abc','IRRELEVANT');
+
+    subject.removeItem('abc');
+
+    expect(locationSpy.search).toHaveBeenCalledWith({abc: null});
+  });
+
+  it('keeps the query param after a route change', function() {
+    locationSpy.path('/irrelevant');
+    subject.setItem('abc','IRRELEVANT');
+
+    locationSpy.path('/irrelevant-change');
+
+    expect(locationSpy.search.calls.mostRecent().args[0]).toEqual({abc: 'IRRELEVANT'});
+  });
+
+  it('discard the query param after a route change when the option removeOnUrlChange is set to true', function() {
+    locationSpy.path('/irrelevant');
+    subject.setItem('abc','IRRELEVANT',{removeOnUrlChange: true});
+
+    locationSpy.path('/irrelevant-change');
+
+    expect(locationSpy.search.calls.mostRecent().args.length).toBe(0);
+  });
+
+  it('discard the query param after a route change when the option removeOnUrlChange is set to true but keeps the other', function() {
+    locationSpy.path('/irrelevant');
+    subject.setItem('abc','IRRELEVANT',{removeOnUrlChange: true});
+    subject.setItem('xyz','IRRELEVANT-STAY',{removeOnUrlChange: false});
+
+    locationSpy.path('/irrelevant-change');
+
+    expect(locationSpy.search.calls.mostRecent().args[0]).toEqual({xyz: 'IRRELEVANT-STAY'});
+  });
+});
