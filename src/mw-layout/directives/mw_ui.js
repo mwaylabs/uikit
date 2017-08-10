ac5a41b849814def2c5eccbb7781fa46f3c46baa
Zarlex <alex.zarges@online.de>
2017-08-10 15:58:36 +0200

    register header and menubar at mw-ui wrapper directive

--- src/mw-layout/directives/mw_ui.js
+++ src/mw-layout/directives/mw_ui.js
@@ -4,9 +4,27 @@ angular.module('mwUI.Layout')
     return {
       transclude: true,
       templateUrl: 'uikit/mw-layout/directives/templates/mw_ui.html',
-      link: function (scope) {
+      controller: function($scope){
+        this.addClass = function(styleClass){
+          $scope.addClass(styleClass);
+        };
+        this.removeClass = function(styleClass){
+          $scope.removeClass(styleClass);
+        };
+      },
+      link: function (scope, el) {
         scope.displayToasts = function(){
           return Modal.getOpenedModals().length === 0;
+        };
+
+        scope.addClass = function(styleClass){
+          console.log('ADD CLASS', styleClass);
+          el.addClass(styleClass)
+        };
+
+        scope.removeClass = function(styleClass){
+          console.log('REMOVE CLASS', styleClass);
+          el.removeClass(styleClass);
         };
       }
     };
