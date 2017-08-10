3953c85dc89e8cd218c82e33779bac47e69175bb
Zarlex <alex.zarges@online.de>
2017-08-10 11:24:14 +0200

    Register class for footer and header so modal can handle different layouts

--- src/mw-modal/directives/mw_modal_footer.js
+++ src/mw-modal/directives/mw_modal_footer.js
@@ -3,6 +3,10 @@ angular.module('mwUI.Modal')
   .directive('mwModalFooter', function () {
     return {
       transclude: true,
-      templateUrl: 'uikit/mw-modal/directives/templates/mw_modal_footer.html'
+      templateUrl: 'uikit/mw-modal/directives/templates/mw_modal_footer.html',
+      require: '^mwModal',
+      link: function(scope, el, attrs, mwModalCtrl){
+        mwModalCtrl.addClass('has-footer');
+      }
     };
   });
