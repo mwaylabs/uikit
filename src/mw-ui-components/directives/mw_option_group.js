f976561239128c51e8895247122b0144d16800f4
Zarlex <alex.zarges@online.de>
2017-08-11 17:44:34 +0200

    Fix #121: Extend options group directive with optional badges

--- src/mw-ui-components/directives/mw_option_group.js
+++ src/mw-ui-components/directives/mw_option_group.js
@@ -6,13 +6,18 @@ angular.module('mwUI.UiComponents')
         title: '@',
         description: '@',
         icon: '@',
-        mwDisabled: '='
+        mwDisabled: '=',
+        badges: '='
       },
       transclude: true,
       templateUrl: 'uikit/mw-ui-components/directives/templates/mw_option_group.html',
       link: function (scope, el) {
         scope.randomId = _.uniqueId('option_group_');
         el.find('input').attr('id', scope.randomId);
+
+        if(scope.badges && !_.isArray(scope.badges)){
+          throw new Error('[mwOptionGroup] The attribute badges only accept an array of strings');
+        }
       }
 
     };
