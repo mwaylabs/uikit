679d42c2104bf7569a37c5abf5c1295a9e6aab05
Zarlex <alex.zarges@online.de>
2017-08-12 16:27:36 +0200

    Fix #78: remove hard coded max-height and calculate it on open

--- src/mw-ui-components/directives/mw_collapsible.js
+++ src/mw-ui-components/directives/mw_collapsible.js
@@ -1,6 +1,6 @@
 angular.module('mwUI.UiComponents')
 
-  //TODO rename to mwCollapsible
+//TODO rename to mwCollapsible
   .directive('mwCollapsable', function () {
     return {
       transclude: true,
@@ -9,15 +9,34 @@ angular.module('mwUI.UiComponents')
         title: '@mwTitle'
       },
       templateUrl: 'uikit/mw-ui-components/directives/templates/mw_collapsible.html',
-      link: function (scope) {
+      link: function (scope, el) {
         scope.viewModel = {};
         scope.viewModel.collapsed = false;
 
+        var getHeight = function (el) {
+          var totalHeight = 0;
+
+          el.children().filter(':visible').each(function () {
+            totalHeight += angular.element(this).outerHeight(true);
+          });
+          return totalHeight;
+        };
+
         scope.toggle = function () {
+          var collapsedBody = el.find('.mw-collapsible-body'),
+            maxHeight;
+
+          if (!scope.viewModel.collapsed) {
+            maxHeight = getHeight(collapsedBody);
+          } else {
+            maxHeight = 0;
+          }
+
+          collapsedBody.css('max-height', maxHeight);
           scope.viewModel.collapsed = !scope.viewModel.collapsed;
         };
 
-        scope.$watch('mwCollapsable', function(){
+        scope.$watch('mwCollapsable', function () {
           if (scope.mwCollapsable === false) {
             scope.viewModel.collapsed = true;
           } else {
--- src/mw-ui-components/directives/mw_collapsible_test.js
+++ src/mw-ui-components/directives/mw_collapsible_test.js
@@ -46,8 +46,8 @@ describe('mwCollapsable', function () {
       expect(isolateScope.viewModel.collapsed).toBe(true);
     });
 
-  describe('define toggle state', function(){
-    beforeEach(function(){
+  describe('define toggle state', function () {
+    beforeEach(function () {
       scope = $rootScope.$new();
       collapsable = '<div mw-collapsable="closed" mw-title="TITLE">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
     });
@@ -100,6 +100,94 @@ describe('mwCollapsable', function () {
         expect(isolateScope.viewModel.collapsed).toBe(false);
       });
 
+  });
+
+  describe('sets max-height when element is opened', function () {
+    afterEach(function(){
+      angular.element('body *[mw-collapsable]').remove();
+    });
+
+    it('when transcluded element has no padding and margin', function () {
+      var transcludedContent = '<div id="testContent">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
+      collapsable = '<div mw-collapsable="closed" mw-title="TITLE">' + transcludedContent + '</div>';
+      scope.closed = true;
+
+      el = $compile(collapsable)(scope);
+      angular.element('body').append(el);
+      scope.$digest();
+      isolateScope = el.isolateScope();
+      isolateScope.toggle();
+      scope.$digest();
+
+      expect(el.find('.mw-collapsible-body').css('max-height')).toBe(el.find('#testContent').innerHeight() + 'px');
+    });
+
+    it('when transcluded element has padding', function () {
+      var transcludedContent = '<div id="testContent" style="padding: 100px">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
+      collapsable = '<div mw-collapsable="closed" mw-title="TITLE">' + transcludedContent + '</div>';
+      scope.closed = true;
+
+      el = $compile(collapsable)(scope);
+      angular.element('body').append(el);
+      scope.$digest();
+      isolateScope = el.isolateScope();
+      isolateScope.toggle();
+      scope.$digest();
+
+      expect(el.find('.mw-collapsible-body').css('max-height')).toBe(el.find('#testContent').innerHeight() + 'px');
+    });
+
+    it('when multiple elements are transcluded', function () {
+      var transcludedContent1 = '<div id="testContent1">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>',
+        transcludedContent2 = '<div id="testContent2">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
+      collapsable = '<div mw-collapsable="closed" mw-title="TITLE">' + transcludedContent1 + transcludedContent2 + '</div>';
+      scope.closed = true;
+
+      el = $compile(collapsable)(scope);
+      angular.element('body').append(el);
+      scope.$digest();
+      isolateScope = el.isolateScope();
+      isolateScope.toggle();
+      scope.$digest();
+
+      var expectedHeight = el.find('#testContent1').innerHeight() + el.find('#testContent2').innerHeight();
+      expect(el.find('.mw-collapsible-body').css('max-height')).toBe(expectedHeight + 'px');
+    });
+
+    it('when multiple elements are transcluded and one of them is hidden', function () {
+      var transcludedContent1 = '<div id="testContent1">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>',
+        transcludedContent2 = '<div id="testContent2" style="display: none">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
+      collapsable = '<div mw-collapsable="closed" mw-title="TITLE">' + transcludedContent1 + transcludedContent2 + '</div>';
+      scope.closed = true;
+
+      el = $compile(collapsable)(scope);
+      angular.element('body').append(el);
+      scope.$digest();
+      isolateScope = el.isolateScope();
+      isolateScope.toggle();
+      scope.$digest();
+
+      var expectedHeight = el.find('#testContent1').innerHeight();
+      expect(el.find('.mw-collapsible-body').css('max-height')).toBe(expectedHeight + 'px');
+    });
+
+    it('when multiple elements are transcluded and they have a margin', function () {
+      var margin = 10,
+        transcludedContent1 = '<div id="testContent1" style="margin: ' + margin + 'px">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>',
+        transcludedContent2 = '<div id="testContent2" style="margin: ' + margin + 'px">TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</div>';
+      collapsable = '<div mw-collapsable="closed" mw-title="TITLE">' + transcludedContent1 + transcludedContent2 + '</div>';
+      scope.closed = true;
+
+      el = $compile(collapsable)(scope);
+      angular.element('body').append(el);
+      scope.$digest();
+      isolateScope = el.isolateScope();
+      isolateScope.toggle();
+      scope.$digest();
+
+      var expectedHeight = el.find('#testContent1').innerHeight() + el.find('#testContent2').innerHeight() + 4 * margin;
+      expect(el.find('.mw-collapsible-body').css('max-height')).toBe(expectedHeight + 'px');
+    });
   });
 
 });
