f908970a86f8a559c9270cd6db8891b350671ca9
Zarlex <alex.zarges@online.de>
2017-08-12 19:53:19 +0200

    Provide directive to set ngModel dirty programmatically on model change

---
+++ src/mw-form/directives/mw_set_dirty_on_model_change.js
@@ -0,0 +1,18 @@
+angular.module('mwUI.Form')
+
+  .directive('mwSetDirtyOnModelChange', function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, el, attrs, ngModelCtrl) {
+        ngModelCtrl.$validators.valueWatcher = function (val) {
+          if (val !== void(0) && val !== null) {
+            ngModelCtrl.$setDirty();
+            ngModelCtrl.$setTouched();
+          } else {
+            ngModelCtrl.$setPristine();
+          }
+          return val;
+        };
+      }
+    }
+  });
