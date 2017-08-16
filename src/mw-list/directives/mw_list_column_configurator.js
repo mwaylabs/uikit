6b66842111675a6535699955dab15c92fa93ab6b
Zarlex <alex.zarges@online.de>
2017-08-16 17:47:43 +0200

    #130 add button to configure columns

---
+++ src/mw-list/directives/mw_list_column_configurator.js
@@ -0,0 +1,44 @@
+angular.module('mwUI.List')
+
+  .directive('mwListColumnConfigurator', function () {
+    return {
+      require: '^mwListableBb',
+      scope: true,
+      templateUrl: 'uikit/mw-list/directives/templates/mw_list_column_configurator.html',
+      link: function (scope, el, attrs, mwListCtrl) {
+        var dropDownMenu = el.find('.dropdown-menu'),
+          dropDownToggle = el.find('.btn-group');
+
+        var hide = function () {
+          if (dropDownToggle.hasClass('open')) {
+            dropDownMenu.dropdown('toggle');
+          }
+          angular.element(window).off('scroll', hide);
+          angular.element(window).off('resize', hide);
+        };
+
+        scope.colums = mwListCtrl.getColumns();
+
+        scope.getColTitle = function (column) {
+          if (column && column.scope) {
+            var title = column.scope.getTitle() || '';
+            if (title.length > 0) {
+              return title;
+            } else {
+              return false;
+            }
+          }
+        };
+
+        dropDownToggle.on('show.bs.dropdown', function () {
+          dropDownMenu.css({
+            position: 'fixed',
+            top: el.offset().top + el.innerHeight() + 5,
+            left: (el.offset().left + el.innerWidth()) - dropDownMenu.innerWidth()
+          });
+          angular.element(window).on('scroll', hide);
+          angular.element(window).on('resize', hide);
+        });
+      }
+    };
+  });
