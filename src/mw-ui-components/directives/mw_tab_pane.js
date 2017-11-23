a857eac76acc6481d748bb058e2966584f157c8b
Zarlex <alex.zarges@online.de>
2017-11-23 11:54:47 +0100

    Do not use ng-if for transcluded content when tab pane is used in a form
    Otherwise validation would not work

--- src/mw-ui-components/directives/mw_tab_pane.js
+++ src/mw-ui-components/directives/mw_tab_pane.js
@@ -32,7 +32,10 @@ angular.module('mwUI.UiComponents')
           return selected;
         };
       },
-      link: function (scope, el, attr, mwTabsCtrl) {
+      link: function (scope, el, attr, ctrls) {
+        var mwTabsCtrl = ctrls[0],
+            formCtrl = ctrls[1];
+
         mwTabsCtrl.registerPane(scope);
 
         scope.$on('$destroy', function () {
@@ -40,6 +43,14 @@ angular.module('mwUI.UiComponents')
           scope.deselect();
           mwTabsCtrl.unRegisterPane(scope);
         });
+
+        // Do not use ng-if to remove the transcluded tab pane content when the tab pane is in a `Form`
+        // Validation does not work when the content is removed from the dom
+        // When you have a tab bar with multiple tabs and each tab contains required input, the tab pane content
+        // must be always in the dom so the form controller knows that the inputs are existing
+        scope.canUseNgIf = function(){
+          return !formCtrl;
+        };
       }
     };
   });
