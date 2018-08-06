2e91566b9ea0c3c791c98c073c584b8555cc535b
Zarlex <alex.zarges@online.de>
2018-08-06 11:04:23 +0200

    Fix #214 select option on header/description text click

--- src/mw-ui-components/directives/mw_option_group.js
+++ src/mw-ui-components/directives/mw_option_group.js
@@ -15,9 +15,16 @@ angular.module('mwUI.UiComponents')
         scope.randomId = _.uniqueId('option_group_');
         el.find('input').attr('id', scope.randomId);
 
-        if(scope.badges && !_.isArray(scope.badges)){
+        if (scope.badges && !_.isArray(scope.badges)) {
           throw new Error('[mwOptionGroup] The attribute badges only accept an array of strings');
         }
+
+        scope.select = function () {
+          var input = el.find('input[type=radio]');
+          if (input) {
+            input.click();
+          }
+        };
       }
 
     };
